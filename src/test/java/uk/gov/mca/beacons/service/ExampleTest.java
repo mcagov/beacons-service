e1878294ba5fe6c85fba7b38e0d96108f52050ae
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-01-20 13:17:51 +0000

    Create example unit test

---
+++ src/test/java/uk/gov/mca/beacons/service/ExampleTest.java
@@ -0,0 +1,15 @@
+package uk.gov.mca.beacons.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class ExampleTest {
+
+  @Test
+  void contextLoads() {
+    assertThat(1, is(1));
+  }
+}
