be68dbda1638b762e88fee074a038ebf56ff3b07
Ninamma Rai <ninamma.rai@outlook.com>
2021-01-20 15:13:06 +0000

    Rename 'Application' class so it's more specific

--- src/test/java/uk/gov/mca/beacons/service/BeaconsRegistrationServiceIntegrationTest.java
+++ src/test/java/uk/gov/mca/beacons/service/BeaconsRegistrationServiceIntegrationTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationIntegrationTest {
-
+class BeaconsRegistrationServiceIntegrationTest {
   @Test
   void contextLoads() {}
 }
