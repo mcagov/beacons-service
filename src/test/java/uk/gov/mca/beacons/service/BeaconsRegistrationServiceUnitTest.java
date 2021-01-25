7fdd7a1dde1757bbcb319fa950efd5ada6d373e2
Ninamma Rai <ninamma.rai@outlook.com>
2021-01-25 11:00:03 +0000

    Add better simple test

--- src/test/java/uk/gov/mca/beacons/service/BeaconsRegistrationServiceUnitTest.java
+++ src/test/java/uk/gov/mca/beacons/service/BeaconsRegistrationServiceUnitTest.java
@@ -1,15 +1,13 @@
 package uk.gov.mca.beacons.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BeaconsRegistrationServiceUnitTest {
 
   @Test
-  void pretendImTests() {
-    assertEquals(true, true);
+  void testShouldCallService() {
+    //    TODO: Remove once we add unit tests
+    final String[] args = {};
+    BeaconsRegistrationService.main(args);
   }
 }
