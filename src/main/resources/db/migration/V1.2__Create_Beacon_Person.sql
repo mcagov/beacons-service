406eef4e652ee78338bbfe885898a3e96cea1f05
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-03 12:32:51 +0000

    Change foreign key types as we're using uuids instead of ints

--- src/main/resources/db/migration/V1.2__Create_Beacon_Person.sql
+++ src/main/resources/db/migration/V1.2__Create_Beacon_Person.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS beacon_person (
     id uuid,
-    beaconId INTEGER REFERENCES beacons (id),
-    personId INTEGER REFERENCES person (id),
+    beaconId uuid REFERENCES beacons (id),
+    personId uuid REFERENCES person (id),
     PRIMARY KEY (id)
 
 )
