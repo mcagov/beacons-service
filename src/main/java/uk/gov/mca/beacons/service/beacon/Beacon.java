f5d23a14e760679772798adaa4959fad9552a7ea
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-04 17:38:12 +0000

    Add remaining fields to Beacon entity and their getters and setters

--- src/main/java/uk/gov/mca/beacons/service/beacon/Beacon.java
+++ src/main/java/uk/gov/mca/beacons/service/beacon/Beacon.java
@@ -1,5 +1,6 @@
 package uk.gov.mca.beacons.service.beacon;
 
+import java.time.LocalDate;
 import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,11 +15,75 @@ public class Beacon {
   @GeneratedValue
   private UUID id;
 
+  private String beaconType;
+  private String hexId;
+  private String manufacturer;
+  private String model;
+  private String serialNumber;
+  private LocalDate batteryExpiry;
+  private LocalDate lastServiced;
+
   public void setId(UUID id) {
     this.id = id;
   }
 
   public UUID getId() {
     return id;
+  }
+
+  public String getBeaconType() {
+    return beaconType;
+  }
+
+  public void setBeaconType(String beaconType) {
+    this.beaconType = beaconType;
+  }
+
+  public String getHexId() {
+    return hexId;
+  }
+
+  public void setHexId(String hexId) {
+    this.hexId = hexId;
+  }
+
+  public String getManufacturer() {
+    return manufacturer;
+  }
+
+  public void setManufacturer(String manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public String getSerialNumber() {
+    return serialNumber;
+  }
+
+  public void setSerialNumber(String serialNumber) {
+    this.serialNumber = serialNumber;
+  }
+
+  public LocalDate getBatteryExpiry() {
+    return batteryExpiry;
+  }
+
+  public void setBatteryExpiry(LocalDate batteryExpiry) {
+    this.batteryExpiry = batteryExpiry;
+  }
+
+  public LocalDate getLastServiced() {
+    return lastServiced;
+  }
+
+  public void setLastServiced(LocalDate lastServiced) {
+    this.lastServiced = lastServiced;
   }
 }
