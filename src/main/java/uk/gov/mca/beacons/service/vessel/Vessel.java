b6586a5e26f8c82b02d200827775dfa2a4d2ac1f
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-04 18:19:30 +0000

    Add Vessel entity and its repository

---
+++ src/main/java/uk/gov/mca/beacons/service/vessel/Vessel.java
@@ -0,0 +1,77 @@
+package uk.gov.mca.beacons.service.vessel;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Vessel {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  private String mmsi;
+  private String name;
+  private String callsign;
+  private String radioComms;
+  private Integer capacity;
+  private String vesselType;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getMmsi() {
+    return mmsi;
+  }
+
+  public void setMmsi(String mmsi) {
+    this.mmsi = mmsi;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCallsign() {
+    return callsign;
+  }
+
+  public void setCallsign(String callsign) {
+    this.callsign = callsign;
+  }
+
+  public String getRadioComms() {
+    return radioComms;
+  }
+
+  public void setRadioComms(String radioComms) {
+    this.radioComms = radioComms;
+  }
+
+  public Integer getCapacity() {
+    return capacity;
+  }
+
+  public void setCapacity(Integer capacity) {
+    this.capacity = capacity;
+  }
+
+  public String getVesselType() {
+    return vesselType;
+  }
+
+  public void setVesselType(String vesselType) {
+    this.vesselType = vesselType;
+  }
+}
