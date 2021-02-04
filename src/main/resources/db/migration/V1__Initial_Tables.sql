285c4e277f4a3a27ef3e30176fb5e5a66992ab51
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-04 10:17:30 +0000

    Remove TODO so it can be captured in an issue

--- src/main/resources/db/migration/V1__Initial_Tables.sql
+++ src/main/resources/db/migration/V1__Initial_Tables.sql
@@ -41,7 +41,6 @@ CREATE TABLE IF NOT EXISTS beacon_uses (
     id uuid PRIMARY KEY,
     beacon_id uuid REFERENCES beacons (id) NOT NULL,
     use_type text,
-    --     TODO: Think about what the best default value would be for main use
     main_use boolean NOT NULL DEFAULT false,
     beacon_person_id uuid REFERENCES beacon_person (id),
     vessel_id uuid REFERENCES vessel (id)
