87e886f4a6ce620c89fbf85092388d6106e64a1c
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-08 11:55:31 +0000

    Enable auditing via annotation configuration, for created and
    last modified dates to be stored

--- src/main/java/uk/gov/mca/beacons/service/BeaconsRegistrationService.java
+++ src/main/java/uk/gov/mca/beacons/service/BeaconsRegistrationService.java
@@ -2,8 +2,10 @@ package uk.gov.mca.beacons.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BeaconsRegistrationService {
 
   public static void main(String[] args) {
