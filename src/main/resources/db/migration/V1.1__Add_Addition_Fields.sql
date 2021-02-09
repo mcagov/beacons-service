138b815dc7e7a02bdf464df0e173b6fcf8ec62c4
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-09 12:04:43 +0000

    Amend migration script with info from Linda

--- src/main/resources/db/migration/V1.1__Add_Addition_Fields.sql
+++ src/main/resources/db/migration/V1.1__Add_Addition_Fields.sql
@@ -1,11 +1,21 @@
 ALTER TABLE beacons
-    ADD COLUMN chk_code text,
+    ADD COLUMN checksum text,
     ADD COLUMN created_date TIMESTAMP,
-    ADD COLUMN last_modified_date TIMESTAMP;
+    ADD COLUMN last_modified_date TIMESTAMP,
+    ADD COLUMN beacon_status text NOT NULL,
+    ADD COLUMN coding text,
+    ADD COLUMN protocol_code text;
 
 ALTER TABLE person
     ADD COLUMN created_date TIMESTAMP,
-    ADD COLUMN last_modified_date TIMESTAMP;
+    ADD COLUMN last_modified_date TIMESTAMP,
+    ADD COLUMN postcode text,
+    ADD COLUMN country text,
+    ADD COLUMN care_of text,
+    DROP COLUMN address_line_5,
+    DROP COLUMN address_line_6,
+    DROP COLUMN address_line_7,
+    DROP COLUMN telephone;
 
 ALTER TABLE beacon_person
     ADD COLUMN created_date TIMESTAMP,
@@ -13,9 +23,28 @@ ALTER TABLE beacon_person
 
 ALTER TABLE vessel
     ADD COLUMN created_date TIMESTAMP,
-    ADD COLUMN last_modified_date TIMESTAMP;
+    ADD COLUMN last_modified_date TIMESTAMP,
+    ADD COLUMN description text,
+    DROP COLUMN mmsi,
+    ADD COLUMN portable_mmsi INTEGER,
+    ADD COLUMN vessel_mmsi INTEGER,
+    ADD COLUMN uksr_id text,
+    ADD COLUMN safetrax_id text,
+    ADD COLUMN homeport text,
+    ADD COLUMN area_of_use text;
+
+ALTER TABLE vessel
+    RENAME capacity TO max_capacity;
 
 ALTER TABLE beacon_uses
-    ADD COLUMN location text,
+    ADD COLUMN beacon_position text,
     ADD COLUMN created_date TIMESTAMP,
     ADD COLUMN last_modified_date TIMESTAMP;
+
+CREATE TABLE IF NOT EXISTS telephone
+(
+    id           uuid PRIMARY KEY,
+    person_id    uuid REFERENCES person (id) NOT NULL,
+    telephone    text,
+    relationship text
+);
