64bfd1321bdf5f020b1932e2b201f587c7d7706c
Matthew Carr <matt.carr@madetech.com>
2021-04-07 14:40:11 +0100

    Fix typo in license

--- src/main/java/uk/gov/mca/beacons/service/configuration/OpenApiConfig.java
+++ src/main/java/uk/gov/mca/beacons/service/configuration/OpenApiConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-  private static final String GITHUB_LICENSE_SUFFIX = "/blob/main/LICENSE";
+  private static final String GITHUB_LICENSE_SUFFIX = "/blob/main/LICENCE";
 
   @Bean
   public OpenAPI beaconsOpenApiConfig(
