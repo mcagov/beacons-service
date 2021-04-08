ad1596a7a9a37119b8bc7d1c25060050948acc31
Matthew Carr <matt.carr@madetech.com>
2021-04-08 20:32:21 +0100

    Adds unit tests

--- src/test/java/uk/gov/mca/beacons/service/registrations/RegistrationsServiceUnitTest.java
+++ src/test/java/uk/gov/mca/beacons/service/registrations/RegistrationsServiceUnitTest.java
@@ -2,11 +2,14 @@ package uk.gov.mca.beacons.service.registrations;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
 import java.util.Collections;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -37,6 +40,7 @@ class RegistrationsServiceUnitTest {
   private BeaconUseRepository beaconUseRepository;
 
   private Registration registration;
+  private UUID beaconId;
   private Beacon beacon;
   private BeaconUse beaconUse;
   private BeaconPerson owner;
@@ -44,7 +48,12 @@ class RegistrationsServiceUnitTest {
 
   @BeforeEach
   void init() {
+    beaconId = UUID.randomUUID();
     beacon = new Beacon();
+    beacon.setId(beaconId);
+    owner = new BeaconPerson();
+    beaconUse = new BeaconUse();
+    emergencyContact = new BeaconPerson();
     beacon.setOwner(owner);
     beacon.setUses(Collections.singletonList(beaconUse));
     beacon.setEmergencyContacts(Collections.singletonList(emergencyContact));
@@ -55,6 +64,8 @@ class RegistrationsServiceUnitTest {
 
   @Test
   void shouldReturnTheSameRegistrationObjectProvided() {
+    given(beaconRepository.save(beacon)).willReturn(beacon);
+
     final Registration expected = registrationsService.register(registration);
 
     assertThat(registration, is(expected));
@@ -67,5 +78,9 @@ class RegistrationsServiceUnitTest {
     registrationsService.register(registration);
 
     then(beaconRepository).should(times(1)).save(beacon);
+    then(beaconUseRepository).should(times(1)).save(beaconUse);
+    then(beaconPersonRepository).should(times(2)).save(isA(BeaconPerson.class));
+    then(beaconPersonRepository).should(times(1)).save(emergencyContact);
+    then(beaconPersonRepository).should(times(1)).save(owner);
   }
 }
