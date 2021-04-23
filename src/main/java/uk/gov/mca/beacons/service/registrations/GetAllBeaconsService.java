f8d8c45f22e4fc180b08407200a3161773f0b153
Tom Maxwell <tom.maxwell@madetech.com>
2021-04-23 09:56:59 +0100

    deal with empty streams better

--- src/main/java/uk/gov/mca/beacons/service/registrations/GetAllBeaconsService.java
+++ src/main/java/uk/gov/mca/beacons/service/registrations/GetAllBeaconsService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -40,33 +41,87 @@ public class GetAllBeaconsService {
   }
 
   public List<Beacon> findAll() {
-    final var allBeacons = new ArrayList<Beacon>();
-    final var result = beaconRepository.findAll();
-    result.forEach(allBeacons::add);
+    final ArrayList<Beacon> allBeacons = getAllBeacons();
+    if (allBeacons.size() == 0)
+      return List.of();
 
-    final var usesStream = StreamSupport.stream(beaconUseRepository.findAll().spliterator(), false);
-    final var groupedUsesByBeacon = usesStream.collect(Collectors.groupingBy(BeaconUse::getBeaconId));
+    final Map<UUID, List<BeaconUse>> groupedUsesByBeacon = getAllUsesGroupedByBeaconId();
 
-    final var personStream = StreamSupport.stream(beaconPersonRepository.findAll().spliterator(), false);
-    final var groupedPersonsByType = personStream.collect(Collectors.groupingBy(BeaconPerson::getPersonType));
-    final var allOwners = groupedPersonsByType.get(PersonType.OWNER);
-    final var emergencyContactSteam = groupedPersonsByType.get(PersonType.EMERGENCY_CONTACT).stream();
-    final var emergencyContactsByBeacon = emergencyContactSteam
-        .collect(Collectors.groupingBy(BeaconPerson::getBeaconId));
+    final Map<PersonType, List<BeaconPerson>> groupedPersonsByType = getAllPersonsGroupedByType();
+    final Map<UUID, BeaconPerson> allOwners = getAllOwnersMappedByBeaconId(groupedPersonsByType);
+    final Map<UUID, List<BeaconPerson>> emergencyContactsByBeacon = getAllContactsGroupedByBeaconId(
+        groupedPersonsByType);
+
+    return mapBeaconRelationships(allBeacons, groupedUsesByBeacon, allOwners, emergencyContactsByBeacon);
+  }
+
+  private List<Beacon> mapBeaconRelationships(final ArrayList<Beacon> allBeacons,
+      final Map<UUID, List<BeaconUse>> usesGroupedByBeacon, final Map<UUID, BeaconPerson> allOwners,
+      final Map<UUID, List<BeaconPerson>> emergencyContactsByBeacon) {
+
+    var mappedBeacons = new ArrayList<Beacon>();
 
     allBeacons.forEach(beacon -> {
-      var beaconUses = groupedUsesByBeacon.get(beacon.getId());
+      var beaconUses = usesGroupedByBeacon.get(beacon.getId());
       beacon.setUses(beaconUses);
-
-      var beaconOwnerOption = allOwners.stream().filter(o -> o.getBeaconId() == beacon.getId()).findFirst();
-      if (!beaconOwnerOption.isEmpty()) {
-        beacon.setOwner(beaconOwnerOption.get());
-      }
+      var beaconOwner = allOwners.get(beacon.getId());
+      // if (allOwners.size() > 0) {
+      // var beaconOwnerOption = allOwners.stream().filter(o -> o.getBeaconId() ==
+      // beacon.getId()).findFirst();
+      // if (!beaconOwnerOption.isEmpty()) {
+      // beacon.setOwner(beaconOwnerOption.get());
+      // }
+      // }
 
       beacon.setEmergencyContacts(emergencyContactsByBeacon.get(beacon.getId()));
 
+      mappedBeacons.add(beacon);
     });
 
+    return mappedBeacons;
+  }
+
+  private Map<UUID, List<BeaconUse>> getAllUsesGroupedByBeaconId() {
+    final var usesStream = StreamSupport.stream(beaconUseRepository.findAll().spliterator(), false);
+    final Map<UUID, List<BeaconUse>> groupedUsesByBeacon = usesStream
+        .collect(Collectors.groupingBy(BeaconUse::getBeaconId));
+    return groupedUsesByBeacon;
+  }
+
+  private Map<UUID, List<BeaconPerson>> getAllContactsGroupedByBeaconId(
+      final Map<PersonType, List<BeaconPerson>> groupedPersonsByType) {
+    final var emergencyContacts = groupedPersonsByType.get(PersonType.EMERGENCY_CONTACT);
+    if (emergencyContacts == null)
+      return new HashMap<UUID, List<BeaconPerson>>();
+
+    final var emergencyContactSteam = emergencyContacts.stream();
+    final Map<UUID, List<BeaconPerson>> emergencyContactsByBeacon = emergencyContactSteam
+        .collect(Collectors.groupingBy(BeaconPerson::getBeaconId));
+    return emergencyContactsByBeacon;
+  }
+
+  private Map<UUID, BeaconPerson> getAllOwnersMappedByBeaconId(
+      final Map<PersonType, List<BeaconPerson>> groupedPersonsByType) {
+    final var owners = groupedPersonsByType.get(PersonType.OWNER);
+    if (owners == null)
+      return new HashMap<UUID, BeaconPerson>();
+
+    var mappedOwners = new HashMap<UUID, BeaconPerson>();
+    owners.forEach(owner -> mappedOwners.putIfAbsent(owner.getBeaconId(), owner));
+
+    return mappedOwners;
+  }
+
+  private Map<PersonType, List<BeaconPerson>> getAllPersonsGroupedByType() {
+    final var personStream = StreamSupport.stream(beaconPersonRepository.findAll().spliterator(), false);
+    final var groupedPersonsByType = personStream.collect(Collectors.groupingBy(BeaconPerson::getPersonType));
+    return groupedPersonsByType;
+  }
+
+  private ArrayList<Beacon> getAllBeacons() {
+    final var allBeacons = new ArrayList<Beacon>();
+    final var result = beaconRepository.findAll();
+    result.forEach(allBeacons::add);
     return allBeacons;
   }
 }
