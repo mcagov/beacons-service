59e02f3f71f8d8741870f13b6e286a4445168117
Matthew Carr <matt.carr@madetech.com>
2021-04-28 13:17:47 +0100

    Remove unused import:

--- src/main/java/uk/gov/mca/beacons/service/registrations/BeaconsController.java
+++ src/main/java/uk/gov/mca/beacons/service/registrations/BeaconsController.java
@@ -2,7 +2,6 @@ package uk.gov.mca.beacons.service.registrations;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
