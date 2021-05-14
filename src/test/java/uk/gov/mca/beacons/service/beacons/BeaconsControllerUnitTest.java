90c3fb3b1343b208b7e16b0bddf525d208b5986e
Matthew Carr <matt.carr@madetech.com>
2021-05-14 11:41:06 +0100

    Remove unused import

--- src/test/java/uk/gov/mca/beacons/service/beacons/BeaconsControllerUnitTest.java
+++ src/test/java/uk/gov/mca/beacons/service/beacons/BeaconsControllerUnitTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
