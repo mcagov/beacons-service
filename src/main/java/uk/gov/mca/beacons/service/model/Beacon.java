b2cddb6a8db35c192d6c631e1ee3240d135a39b9
Matthew Carr <76042279+matthew-a-carr@users.noreply.github.com>
2021-06-15 09:00:25 +0100

    Add Lombok integration and update README (#80)
    * Add Lombok integration and remove getters/setters in favour of annotaitons
    
    * Add getter on field rather than class level
    
    * Remove * import
    
    * Tweak README

--- src/main/java/uk/gov/mca/beacons/service/model/Beacon.java
+++ src/main/java/uk/gov/mca/beacons/service/model/Beacon.java
@@ -14,11 +14,15 @@ import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
 public class Beacon {
 
   @Id
@@ -64,116 +68,4 @@ public class Beacon {
   @NotEmpty
   @Valid
   private List<BeaconPerson> emergencyContacts;
-
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public String getHexId() {
-    return hexId;
-  }
-
-  public void setHexId(String hexId) {
-    this.hexId = hexId;
-  }
-
-  public String getManufacturer() {
-    return manufacturer;
-  }
-
-  public void setManufacturer(String manufacturer) {
-    this.manufacturer = manufacturer;
-  }
-
-  public String getModel() {
-    return model;
-  }
-
-  public void setModel(String model) {
-    this.model = model;
-  }
-
-  public String getManufacturerSerialNumber() {
-    return manufacturerSerialNumber;
-  }
-
-  public void setManufacturerSerialNumber(String manufacturerSerialNumber) {
-    this.manufacturerSerialNumber = manufacturerSerialNumber;
-  }
-
-  public String getReferenceNumber() {
-    return referenceNumber;
-  }
-
-  public void setReferenceNumber(String reference) {
-    this.referenceNumber = reference;
-  }
-
-  public String getChkCode() {
-    return chkCode;
-  }
-
-  public void setChkCode(String chkCode) {
-    this.chkCode = chkCode;
-  }
-
-  public LocalDate getBatteryExpiryDate() {
-    return batteryExpiryDate;
-  }
-
-  public void setBatteryExpiryDate(LocalDate batteryExpiryDate) {
-    this.batteryExpiryDate = batteryExpiryDate;
-  }
-
-  public LocalDate getLastServicedDate() {
-    return lastServicedDate;
-  }
-
-  public void setLastServicedDate(LocalDate lastServicedDate) {
-    this.lastServicedDate = lastServicedDate;
-  }
-
-  public BeaconStatus getBeaconStatus() {
-    return beaconStatus;
-  }
-
-  public void setBeaconStatus(BeaconStatus beaconStatus) {
-    this.beaconStatus = beaconStatus;
-  }
-
-  public LocalDateTime getCreatedDate() {
-    return createdDate;
-  }
-
-  public void setCreatedDate(LocalDateTime createdDate) {
-    this.createdDate = createdDate;
-  }
-
-  public List<BeaconUse> getUses() {
-    return uses;
-  }
-
-  public void setUses(List<BeaconUse> uses) {
-    this.uses = uses;
-  }
-
-  public BeaconPerson getOwner() {
-    return owner;
-  }
-
-  public void setOwner(BeaconPerson owner) {
-    this.owner = owner;
-  }
-
-  public List<BeaconPerson> getEmergencyContacts() {
-    return emergencyContacts;
-  }
-
-  public void setEmergencyContacts(List<BeaconPerson> emergencyContacts) {
-    this.emergencyContacts = emergencyContacts;
-  }
 }
