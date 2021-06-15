c04d3c2af7e120ab7e84ae7c1efbda11f3c19542
Matthew Carr <matt.carr@madetech.com>
2021-06-15 08:01:15 +0100

    Remove * import

--- src/main/java/uk/gov/mca/beacons/service/model/AccountHolder.java
+++ src/main/java/uk/gov/mca/beacons/service/model/AccountHolder.java
@@ -1,7 +1,10 @@
 package uk.gov.mca.beacons.service.model;
 
 import java.util.UUID;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
