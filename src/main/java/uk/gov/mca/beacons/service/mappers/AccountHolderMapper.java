9bf65259873070a39b7dde3dd76a512b2b1d5137
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-06-16 11:02:19 +0100

    Implement AccountHolderMapper#toWrapperDTO

--- src/main/java/uk/gov/mca/beacons/service/mappers/AccountHolderMapper.java
+++ src/main/java/uk/gov/mca/beacons/service/mappers/AccountHolderMapper.java
@@ -2,13 +2,30 @@ package uk.gov.mca.beacons.service.mappers;
 
 import org.springframework.stereotype.Service;
 import uk.gov.mca.beacons.service.dto.AccountHolderDTO;
+import uk.gov.mca.beacons.service.dto.WrapperDTO;
 import uk.gov.mca.beacons.service.model.AccountHolder;
 
 @Service
 public class AccountHolderMapper extends BaseMapper {
 
   public AccountHolder fromDTO(AccountHolderDTO accountHolderDTO) {
-    return null;
+    final var accountHolder = new AccountHolder();
+    accountHolder.setId(accountHolderDTO.getId());
+
+    accountHolder.setAuthId((String) accountHolderDTO.getAttributes().get("authId"));
+    accountHolder.setEmail((String) accountHolderDTO.getAttributes().get("email"));
+    accountHolder.setFullName((String) accountHolderDTO.getAttributes().get("fullName"));
+    accountHolder.setTelephoneNumber((String) accountHolderDTO.getAttributes().get("telephoneNumber"));
+    accountHolder.setAlternativeTelephoneNumber((String) accountHolderDTO.getAttributes().get("alternativeTelephoneNumber"));
+    accountHolder.setAddressLine1((String) accountHolderDTO.getAttributes().get("addressLine1"));
+    accountHolder.setAddressLine2((String) accountHolderDTO.getAttributes().get("addressLine2"));
+    accountHolder.setAddressLine3((String) accountHolderDTO.getAttributes().get("addressLine3"));
+    accountHolder.setAddressLine4((String) accountHolderDTO.getAttributes().get("addressLine4"));
+    accountHolder.setTownOrCity((String) accountHolderDTO.getAttributes().get("townOrCity"));
+    accountHolder.setPostcode((String) accountHolderDTO.getAttributes().get("postcode"));
+    accountHolder.setCounty((String) accountHolderDTO.getAttributes().get("county"));
+
+    return accountHolder;
   }
 
   public AccountHolderDTO toDTO(AccountHolder accountHolder) {
@@ -32,4 +49,14 @@ public class AccountHolderMapper extends BaseMapper {
 
     return dto;
   }
+
+  public WrapperDTO<AccountHolderDTO> toWrapperDTO(AccountHolder accountHolder) {
+    WrapperDTO<AccountHolderDTO> wrapperDTO = new WrapperDTO<>();
+
+    wrapperDTO.setData(toDTO(accountHolder));
+
+    return wrapperDTO;
+  }
+
+
 }
--- src/test/java/uk/gov/mca/beacons/service/accounts/AccountHolderMapperUnitTest.java
+++ src/test/java/uk/gov/mca/beacons/service/accounts/AccountHolderMapperUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.mca.beacons.service.dto.AccountHolderDTO;
+import uk.gov.mca.beacons.service.dto.WrapperDTO;
 import uk.gov.mca.beacons.service.hateoas.BeaconLinkStrategy;
 import uk.gov.mca.beacons.service.hateoas.HateoasLinkManager;
 import uk.gov.mca.beacons.service.mappers.AccountHolderMapper;
@@ -15,6 +16,7 @@ import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class AccountHolderMapperUnitTest {
@@ -92,5 +94,82 @@ class AccountHolderMapperUnitTest {
             accountHolderDTO.getAttributes().get("county"),
             is("Testershire")
     );
+  }
+
+  @Test
+  void fromDTO_shouldInstantiateADomainObjectFromTheDTO() {
+    var accountHolderDTO = new AccountHolderDTO();
+    accountHolderDTO.setId(UUID.fromString("461fc925-dbe0-448b-acf4-18727958393e"));
+    accountHolderDTO.addAttribute("authId", "a2fb6bb2-f735-41aa-a4a1-19cb951a51bc");
+    accountHolderDTO.addAttribute("email", "testy@mctestface.com");
+    accountHolderDTO.addAttribute("fullName", "Tesy McTestface");
+    accountHolderDTO.addAttribute("telephoneNumber", "01178 657123");
+    accountHolderDTO.addAttribute("alternativeTelephoneNumber", "");
+    accountHolderDTO.addAttribute("addressLine1", "Flat 42");
+    accountHolderDTO.addAttribute("addressLine2", "Testington Towers");
+    accountHolderDTO.addAttribute("addressLine3", "");
+    accountHolderDTO.addAttribute("addressLine4", "");
+    accountHolderDTO.addAttribute("townOrCity", "Testville");
+    accountHolderDTO.addAttribute("postcode", "TS1 23A");
+    accountHolderDTO.addAttribute("county", "Testershire");
+    var accountHolderMapper = new AccountHolderMapper();
+
+    AccountHolder accountHolder = accountHolderMapper.fromDTO(accountHolderDTO);
+
+    assertThat(
+            accountHolder.getId(),
+            is(UUID.fromString("461fc925-dbe0-448b-acf4-18727958393e"))
+    );
+    assertThat(
+            accountHolder.getAuthId(),
+            is("a2fb6bb2-f735-41aa-a4a1-19cb951a51bc")
+    );
+    assertThat(
+            accountHolder.getEmail(),
+            is("testy@mctestface.com")
+    );
+    assertThat(
+            accountHolder.getFullName(),
+            is("Tesy McTestface")
+    );
+    assertThat(
+            accountHolder.getTelephoneNumber(),
+            is("01178 657123")
+    );
+    assertThat(
+            accountHolder.getAlternativeTelephoneNumber(),
+            is("")
+    );
+    assertThat(
+            accountHolder.getAddressLine1(),
+            is("Flat 42")
+    );
+    assertThat(
+            accountHolder.getAddressLine2(),
+            is("Testington Towers")
+    );
+    assertThat(accountHolder.getAddressLine3(), is(""));
+    assertThat(accountHolder.getAddressLine4(), is(""));
+    assertThat(
+            accountHolder.getTownOrCity(),
+            is("Testville")
+    );
+    assertThat(accountHolder.getPostcode(), is("TS1 23A"));
+    assertThat(
+            accountHolder.getCounty(),
+            is("Testershire")
+    );
+  }
+
+  @Test
+  void toWrapperDTO_shouldConvertAnAccountHolderToAWrappedDTO() {
+    var accountHolder = new AccountHolder();
+    var accountHolderMapper = new AccountHolderMapper();
+
+    WrapperDTO<AccountHolderDTO> wrappedAccountHolder = accountHolderMapper.toWrapperDTO(accountHolder);
+
+    assertNotNull(wrappedAccountHolder.getData());
+    assertNotNull(wrappedAccountHolder.getIncluded());
+    assertNotNull(wrappedAccountHolder.getMeta());
   }
 }
