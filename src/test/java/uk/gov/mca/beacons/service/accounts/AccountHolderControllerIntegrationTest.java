2514d0e43acf8444c35388f91f8d551792d1e91e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-06-16 14:09:26 +0100

    Remove repository mock + replace with randomising authId in test

--- src/test/java/uk/gov/mca/beacons/service/accounts/AccountHolderControllerIntegrationTest.java
+++ src/test/java/uk/gov/mca/beacons/service/accounts/AccountHolderControllerIntegrationTest.java
@@ -1,8 +1,5 @@
 package uk.gov.mca.beacons.service.accounts;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -10,13 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
-import uk.gov.mca.beacons.service.model.AccountHolder;
-import uk.gov.mca.beacons.service.repository.AccountHolderRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -25,43 +19,24 @@ class AccountHolderControllerIntegrationTest {
   @Autowired
   private WebTestClient webTestClient;
 
-  @MockBean
-  private AccountHolderRepository accountHolderRepository;
-
   @Test
   void requestCreateAccountHolder_shouldRespondWithTheCreatedResource()
     throws Exception {
+    String testAuthId = UUID.randomUUID().toString();
     String newAccountHolderRequest = new String(
       Files.readAllBytes(
         Paths.get("src/test/resources/fixtures/createAccountHolderRequest.json")
       )
-    );
+    )
+      .replace("replace-with-test-auth-id", testAuthId);
     String expectedResponse = new String(
       Files.readAllBytes(
         Paths.get(
           "src/test/resources/fixtures/createAccountHolderResponse.json"
         )
       )
-    );
-    AccountHolder accountHolder = new AccountHolder();
-    accountHolder.setId(
-      UUID.fromString("b81c2419-91b7-49de-b273-98206e02b739")
-    );
-    accountHolder.setAuthId("461fc925-dbe0-448b-acf4-18727958393e");
-    accountHolder.setEmail("testy@mctestface.com");
-    accountHolder.setFullName("Tesy McTestface");
-    accountHolder.setTelephoneNumber("01178 657123");
-    accountHolder.setAlternativeTelephoneNumber("");
-    accountHolder.setAddressLine1("Flat 42");
-    accountHolder.setAddressLine2("Testington Towers");
-    accountHolder.setAddressLine3("");
-    accountHolder.setAddressLine4("");
-    accountHolder.setTownOrCity("Testville");
-    accountHolder.setPostcode("TS1 23A");
-    accountHolder.setCounty("Testershire");
-
-    given(accountHolderRepository.save(any(AccountHolder.class)))
-      .willReturn(accountHolder);
+    )
+      .replace("replace-with-test-auth-id", testAuthId);
 
     webTestClient
       .post()
