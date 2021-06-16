d1768ef13712fc9f30c5bcacedc26ad53b538ac4
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-06-16 11:20:39 +0100

    Map AccountHolder properties to correct table names

--- src/main/java/uk/gov/mca/beacons/service/model/AccountHolder.java
+++ src/main/java/uk/gov/mca/beacons/service/model/AccountHolder.java
@@ -1,10 +1,15 @@
 package uk.gov.mca.beacons.service.model;
 
-import java.util.UUID;
-import javax.persistence.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Entity
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity(name = "beacon_account_holder")
 @EntityListeners(AuditingEntityListener.class)
 public class AccountHolder {
 
@@ -22,12 +27,16 @@ public class AccountHolder {
 
   private String alternativeTelephoneNumber;
 
+  @Column(name = "address_line_1")
   private String addressLine1;
 
+  @Column(name = "address_line_2")
   private String addressLine2;
 
+  @Column(name = "address_line_3")
   private String addressLine3;
 
+  @Column(name = "address_line_4")
   private String addressLine4;
 
   private String townOrCity;
