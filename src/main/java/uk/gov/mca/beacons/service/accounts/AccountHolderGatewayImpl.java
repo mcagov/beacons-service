fc1cd90c30aa2c3698f829e1c70cd569e881ad5b
Matthew Carr <matt.carr@madetech.com>
2021-06-22 09:07:34 +0100

    Add SQL for getting account holder by auth id

--- src/main/java/uk/gov/mca/beacons/service/accounts/AccountHolderGatewayImpl.java
+++ src/main/java/uk/gov/mca/beacons/service/accounts/AccountHolderGatewayImpl.java
@@ -13,44 +13,69 @@ import uk.gov.mca.beacons.service.domain.AccountHolder;
 @Transactional
 public class AccountHolderGatewayImpl implements AccountHolderGateway {
 
-    private final NamedParameterJdbcTemplate jdbcTemplate;
+  private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public AccountHolderGatewayImpl(NamedParameterJdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+  @Autowired
+  public AccountHolderGatewayImpl(NamedParameterJdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
 
-    @Override
-    public AccountHolder getById(UUID id) {
-        final SqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
-        return jdbcTemplate.queryForObject(
-                "SELECT " +
-                        "account_holder.id, " +
-                        "account_holder.auth_id, " +
-                        "person.full_name, " +
-                        "person.email, " +
-                        "person.address_line_1 as address_line1, " +
-                        "person.address_line_2 as address_line2, " +
-                        "person.address_line_3 as address_line3, " +
-                        "person.address_line_4 as address_line4, " +
-                        "person.postcode, " +
-                        "person.county, " +
-                        "person.telephone_number, " +
-                        "person.alternative_telephone_number, " +
-                        "person.town_or_city " +
-                        "FROM account_holder " +
-                        "INNER JOIN person ON account_holder.person_id = person.id " +
-                        "WHERE account_holder.id = :id", params, AccountHolder.class
-        );
-    }
+  @Override
+  public AccountHolder getById(UUID id) {
+    final SqlParameterSource params = new MapSqlParameterSource()
+      .addValue("id", id);
+    return jdbcTemplate.queryForObject(
+      "SELECT " +
+      "account_holder.id, " +
+      "account_holder.auth_id, " +
+      "person.full_name, " +
+      "person.email, " +
+      "person.address_line_1 as address_line1, " +
+      "person.address_line_2 as address_line2, " +
+      "person.address_line_3 as address_line3, " +
+      "person.address_line_4 as address_line4, " +
+      "person.postcode, " +
+      "person.county, " +
+      "person.telephone_number, " +
+      "person.alternative_telephone_number, " +
+      "person.town_or_city " +
+      "FROM account_holder " +
+      "INNER JOIN person ON account_holder.person_id = person.id " +
+      "WHERE account_holder.id = :id",
+      params,
+      AccountHolder.class
+    );
+  }
 
-    @Override
-    public AccountHolder getByAuthId(String authId) {
-        return null;
-    }
+  @Override
+  public AccountHolder getByAuthId(String authId) {
+    final SqlParameterSource params = new MapSqlParameterSource()
+      .addValue("authId", authId);
+    return jdbcTemplate.queryForObject(
+      "SELECT " +
+      "account_holder.id, " +
+      "account_holder.auth_id, " +
+      "person.full_name, " +
+      "person.email, " +
+      "person.address_line_1 as address_line1, " +
+      "person.address_line_2 as address_line2, " +
+      "person.address_line_3 as address_line3, " +
+      "person.address_line_4 as address_line4, " +
+      "person.postcode, " +
+      "person.county, " +
+      "person.telephone_number, " +
+      "person.alternative_telephone_number, " +
+      "person.town_or_city " +
+      "FROM account_holder " +
+      "INNER JOIN person ON account_holder.person_id = person.id " +
+      "WHERE account_holder.auth_id = :authId",
+      params,
+      AccountHolder.class
+    );
+  }
 
-    @Override
-    public AccountHolder save(AccountHolder accountHolder) {
-        return null;
-    }
+  @Override
+  public AccountHolder save(AccountHolder accountHolder) {
+    return null;
+  }
 }
