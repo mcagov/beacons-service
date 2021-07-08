e16c2a2575f2626d6b9ab5140834eb52a3fc100b
Ninamma Rai <ninamma.rai@outlook.com>
2021-07-08 17:55:41 +0100

    Remove comments and use builder :bricks:

--- src/main/java/uk/gov/mca/beacons/api/mappers/NoteMapper.java
+++ src/main/java/uk/gov/mca/beacons/api/mappers/NoteMapper.java
@@ -9,43 +9,30 @@ public class NoteMapper {
   public static NoteEntity toNoteEntity(Note note) {
     final LocalDateTime now = LocalDateTime.now();
 
-    NoteEntity noteEntity = new NoteEntity();
-    noteEntity.setId(note.getId());
-    noteEntity.setBeaconId(note.getBeaconId());
-    noteEntity.setText(note.getText());
-    noteEntity.setType(note.getType());
-    noteEntity.setCreatedDate(
-      note.getCreatedDate() == null ? now : note.getCreatedDate()
-    );
-    noteEntity.setPersonId(note.getPersonId());
-    noteEntity.setFullName(note.getFullName());
-    noteEntity.setEmail(note.getEmail());
-
-    return noteEntity;
+    return NoteEntity
+      .builder()
+      .id(note.getId())
+      .beaconId(note.getBeaconId())
+      .text(note.getText())
+      .type(note.getType())
+      .createdDate(note.getCreatedDate() == null ? now : note.getCreatedDate())
+      .personId(note.getPersonId())
+      .fullName(note.getFullName())
+      .email(note.getEmail())
+      .build();
   }
 
   public static Note fromNoteEntity(NoteEntity noteEntity) {
-    Note note = new Note();
-
-    note.setId(noteEntity.getId());
-    note.setBeaconId(noteEntity.getBeaconId());
-    note.setText(noteEntity.getText());
-    note.setType(noteEntity.getType());
-    note.setCreatedDate(noteEntity.getCreatedDate());
-    note.setPersonId(noteEntity.getPersonId());
-    note.setFullName(noteEntity.getFullName());
-    note.setEmail(noteEntity.getEmail());
-
-    return note;
-    //      .builder()
-    //      .id(noteEntity.getId())
-    //      .beaconId(noteEntity.getBeaconId())
-    //      .text(noteEntity.getText())
-    //      .type(noteEntity.getType())
-    //      .createdDate(noteEntity.getCreatedDate())
-    //      .personId(noteEntity.getPersonId())
-    //      .fullName(noteEntity.getFullName())
-    //      .email(noteEntity.getEmail())
-    //      .build();
+    return Note
+      .builder()
+      .id(noteEntity.getId())
+      .beaconId(noteEntity.getBeaconId())
+      .text(noteEntity.getText())
+      .type(noteEntity.getType())
+      .createdDate(noteEntity.getCreatedDate())
+      .personId(noteEntity.getPersonId())
+      .fullName(noteEntity.getFullName())
+      .email(noteEntity.getEmail())
+      .build();
   }
 }
