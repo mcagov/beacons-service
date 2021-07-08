869ee3c4931980cf68d45b43b49e14ebf199a5c5
Ninamma Rai <ninamma.rai@outlook.com>
2021-07-08 18:02:13 +0100

    Add Note Service - only create method right now. Also annotations!

--- src/main/java/uk/gov/mca/beacons/api/gateways/NoteGatewayImpl.java
+++ src/main/java/uk/gov/mca/beacons/api/gateways/NoteGatewayImpl.java
@@ -1,10 +1,14 @@
 package uk.gov.mca.beacons.api.gateways;
 
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 import uk.gov.mca.beacons.api.domain.Note;
 import uk.gov.mca.beacons.api.jpa.NoteJpaRepository;
 import uk.gov.mca.beacons.api.jpa.entities.NoteEntity;
 import uk.gov.mca.beacons.api.mappers.NoteMapper;
 
+@Repository
+@Transactional
 public class NoteGatewayImpl implements NoteGateway {
 
   private NoteJpaRepository noteJpaRepository;
