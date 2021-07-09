5fd12a88b0d81cc40d1740a3c5335c2530d5e6b1
Matthew Carr <matt.carr@madetech.com>
2021-07-09 13:32:41 +0100

    Remove unused code

--- src/main/java/uk/gov/mca/beacons/api/domain/User.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.mca.beacons.api.domain;
-
-public interface User {
-  String getId();
-  String getFullName();
-  String getEmail();
-}
--- src/main/java/uk/gov/mca/beacons/api/gateways/CurrentUserGateway.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.mca.beacons.api.gateways;
-
-import uk.gov.mca.beacons.api.domain.User;
-
-public interface CurrentUserGateway {
-  User getCurrentUser();
-}
