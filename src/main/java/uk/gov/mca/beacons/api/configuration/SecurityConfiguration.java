6de2821d5ca255cbaf7fbb24540c070948f5257a
Matthew Carr <matt.carr@madetech.com>
2021-07-16 13:29:47 +0100

    Add basic auth over migration endpoint and create migration controller

--- src/main/java/uk/gov/mca/beacons/api/configuration/SecurityConfiguration.java
+++ src/main/java/uk/gov/mca/beacons/api/configuration/SecurityConfiguration.java
@@ -1,32 +1,91 @@
 package uk.gov.mca.beacons.api.configuration;
 
 import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration
-  extends AADResourceServerWebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http);
-    http.cors().and().authorizeRequests().antMatchers("/**").authenticated();
+  /**
+   * Custom Security configuration that secures migration endpoints over basic auth rather than Azure AD.
+   */
+  @Order(1)
+  @Configuration
+  public static class BasicAuthConfiguration
+    extends WebSecurityConfigurerAdapter {
+
+    @Value("${beacons.security.basic.user.name}")
+    private String username;
+
+    @Value("${beacons.security.basic.user.password}")
+    private String password;
+
+    /**
+     * Creates a HTTP basic auth security filter for the migration endpoint.
+     *
+     * NOTE: The HTTP session has to be stateless for the basic auth security filter.
+     * Otherwise Spring allows access to Azure authenticated endpoints if a user
+     * has a session through authenticating via basic auth.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .cors()
+        .and()
+        .antMatcher("/migrate/**")
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+      throws Exception {
+      auth
+        .inMemoryAuthentication()
+        .withUser(username)
+        .password("{noop}" + password)
+        .roles("migration");
+    }
   }
 
-  @Override
-  public void configure(WebSecurity web) {
-    web
-      .ignoring()
-      .antMatchers(
-        "/owner",
-        "/actuator/health",
-        "/actuator/info",
-        "/swagger-ui**",
-        "/v3/api-docs/**"
-      );
+  @Order(2)
+  @Configuration
+  public static class AzureAdSecurityConfiguration
+    extends AADResourceServerWebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      super.configure(http);
+      http.cors().and().authorizeRequests().antMatchers("/**").authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+      web
+        .ignoring()
+        .antMatchers(
+          "/actuator/health",
+          "/actuator/info",
+          "/swagger-ui**",
+          "/v3/api-docs/**"
+        );
+    }
   }
 }
---
+++ src/main/java/uk/gov/mca/beacons/api/controllers/MigrationController.java
@@ -0,0 +1,19 @@
+package uk.gov.mca.beacons.api.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/migrate")
+@Tag(
+  name = "Migration Controller for existing beacon records, owners, emergency contacts, and uses"
+)
+public class MigrationController {
+
+  @GetMapping(value = "/")
+  public String base() {
+    return "Migration Controller";
+  }
+}
