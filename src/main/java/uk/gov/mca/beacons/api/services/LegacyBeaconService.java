af14f9133e8ac3ff795651aa6c4d50e8ab6e86d1
Matthew Carr <matt.carr@madetech.com>
2021-08-03 08:51:08 +0100

    Remove unused import

--- src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
+++ src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.mca.beacons.api.domain.LegacyBeacon;
-import uk.gov.mca.beacons.api.dto.LegacyBeaconDTO;
 import uk.gov.mca.beacons.api.gateways.LegacyBeaconGateway;
 
 @Service
