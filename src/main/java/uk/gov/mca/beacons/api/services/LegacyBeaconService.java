c0b11ec2740dc0c845823567ce4df29516a0f8ec
Matthew Carr <76042279+matthew-a-carr@users.noreply.github.com>
2021-08-05 11:48:38 +0100

    Legacy beacon validation (#137)
    * Add spring bean validation on maps
    
    * TDD other validation rules for legacy  beacon
    
    * Add integration tests for validating hex id, created date, and last modified date
    
    * Update javadoc for validation exception

--- src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
+++ src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
@@ -3,22 +3,40 @@ package uk.gov.mca.beacons.api.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BeanPropertyBindingResult;
 import uk.gov.mca.beacons.api.domain.LegacyBeacon;
+import uk.gov.mca.beacons.api.exceptions.BeaconsValidationException;
 import uk.gov.mca.beacons.api.gateways.LegacyBeaconGateway;
+import uk.gov.mca.beacons.api.services.validation.LegacyBeaconValidator;
 
 @Service
 @Transactional
 public class LegacyBeaconService {
 
   private final LegacyBeaconGateway legacyBeaconGateway;
+  private final LegacyBeaconValidator legacyBeaconValidator;
 
   @Autowired
-  public LegacyBeaconService(LegacyBeaconGateway legacyBeaconGateway) {
+  public LegacyBeaconService(
+    LegacyBeaconGateway legacyBeaconGateway,
+    LegacyBeaconValidator legacyBeaconValidator
+  ) {
     this.legacyBeaconGateway = legacyBeaconGateway;
+    this.legacyBeaconValidator = legacyBeaconValidator;
   }
 
   public LegacyBeacon create(LegacyBeacon beacon) {
+    validate(beacon);
     return legacyBeaconGateway.save(beacon);
+  }
+
+  private void validate(LegacyBeacon beacon) {
+    final var errors = new BeanPropertyBindingResult(beacon, "legacyBeacon");
+    legacyBeaconValidator.validate(beacon, errors);
+
+    if (errors.hasFieldErrors()) {
+      throw new BeaconsValidationException(errors);
+    }
   }
 
   public void deleteAll() {
