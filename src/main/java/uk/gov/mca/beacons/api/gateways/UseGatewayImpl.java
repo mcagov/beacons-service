73e9f7dcce0db9b4d8e74f803cb36737473c2dc1
Matthew Carr <matt.carr@madetech.com>
2021-08-05 17:10:47 +0100

    Prefer Spring @Transactional over javax

--- src/main/java/uk/gov/mca/beacons/api/gateways/UseGatewayImpl.java
+++ src/main/java/uk/gov/mca/beacons/api/gateways/UseGatewayImpl.java
@@ -2,9 +2,9 @@ package uk.gov.mca.beacons.api.gateways;
 
 import java.util.List;
 import java.util.UUID;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.mca.beacons.api.jpa.BeaconUseJpaRepository;
 import uk.gov.mca.beacons.api.jpa.entities.BeaconUse;
 
--- src/main/java/uk/gov/mca/beacons/api/services/AccountHolderService.java
+++ src/main/java/uk/gov/mca/beacons/api/services/AccountHolderService.java
@@ -1,9 +1,9 @@
 package uk.gov.mca.beacons.api.services;
 
 import java.util.UUID;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.mca.beacons.api.domain.AccountHolder;
 import uk.gov.mca.beacons.api.dto.CreateAccountHolderRequest;
 import uk.gov.mca.beacons.api.gateways.AccountHolderGateway;
