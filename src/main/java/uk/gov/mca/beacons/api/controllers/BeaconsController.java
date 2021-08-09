f50ba6efba620d7dbb92f5c84536bdd68f8fd8ed
Matthew Carr <matt.carr@madetech.com>
2021-08-09 16:00:31 +0100

    Oragnise imports in BeaconsController

--- src/main/java/uk/gov/mca/beacons/api/controllers/BeaconsController.java
+++ src/main/java/uk/gov/mca/beacons/api/controllers/BeaconsController.java
@@ -1,7 +1,6 @@
 package uk.gov.mca.beacons.api.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -12,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.mca.beacons.api.domain.Note;
-import uk.gov.mca.beacons.api.dto.*;
+import uk.gov.mca.beacons.api.dto.BeaconDTO;
+import uk.gov.mca.beacons.api.dto.BeaconSearchResultDTO;
+import uk.gov.mca.beacons.api.dto.DeleteBeaconRequestDTO;
+import uk.gov.mca.beacons.api.dto.NoteDTO;
+import uk.gov.mca.beacons.api.dto.WrapperDTO;
 import uk.gov.mca.beacons.api.exceptions.InvalidBeaconDeleteException;
 import uk.gov.mca.beacons.api.exceptions.InvalidPatchException;
 import uk.gov.mca.beacons.api.mappers.BeaconMapper;
