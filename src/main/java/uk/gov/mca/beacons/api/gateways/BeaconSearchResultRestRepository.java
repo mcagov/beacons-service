fede5b05a4469b9f5f3a004141a956247fa36f52
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-08-11 12:08:44 +0100

    WIP - Added @RepositoryRestResource annotation

---
+++ src/main/java/uk/gov/mca/beacons/api/gateways/BeaconSearchResultRestRepository.java
@@ -0,0 +1,8 @@
+package uk.gov.mca.beacons.api.gateways;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public class BeaconSearchResultRestRepository {
+
+}
