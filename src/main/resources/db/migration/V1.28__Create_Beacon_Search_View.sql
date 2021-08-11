19bee4fd3253d57bfcfa29a419249e0ec8b70ee3
Matthew Carr <matt.carr@madetech.com>
2021-08-11 15:03:02 +0100

    Update comments on flyway migration

--- src/main/resources/db/migration/V1.28__Create_Beacon_Search_View.sql
+++ src/main/resources/db/migration/V1.28__Create_Beacon_Search_View.sql
@@ -1,4 +1,4 @@
--- Create a union
+-- Create a single view of beacons from the beacon and legacy_beacon tables
 CREATE OR REPLACE VIEW beacon_search AS
     SELECT id, hex_id FROM beacon
         UNION
