860a94572f296b6f0f16190854a1166a7e126cb8
Matthew Carr <matt.carr@madetech.com>
2021-08-16 22:24:18 +0100

    Add triggers for refreshing materialized view

--- src/main/resources/db/migration/V1.28__Create_Beacon_Search_View.sql
+++ src/main/resources/db/migration/V1.28__Create_Beacon_Search_View.sql
@@ -17,3 +17,25 @@ CREATE MATERIALIZED VIEW beacon_search_mat AS
     FROM legacy_beacon;
 
 CREATE UNIQUE INDEX beacon_search_mat_id ON beacon_search_mat(id);
+
+-- Create function to refresh the beacon search materialized view
+CREATE OR REPLACE FUNCTION refresh_beacon_search_view()
+    RETURNS TRIGGER LANGUAGE plpgsql
+AS $$
+BEGIN
+    REFRESH MATERIALIZED VIEW CONCURRENTLY beacon_search_mat;
+END $$;
+
+-- Create a trigger to refresh the beacon search materialized view on inserts into the legacy beacon table
+CREATE TRIGGER refresh_beacon_search_on_insert_into_legacy_beacons
+    AFTER INSERT
+    ON legacy_beacon
+    FOR EACH STATEMENT
+    EXECUTE PROCEDURE refresh_beacon_search_view();
+
+-- Create a trigger to refresh the beacon search materialized view on inserts into the legacy beacon table
+CREATE TRIGGER refresh_beacon_search_on_insert_into_beacons
+    AFTER INSERT
+    ON beacon
+    FOR EACH STATEMENT
+    EXECUTE PROCEDURE refresh_beacon_search_view();
