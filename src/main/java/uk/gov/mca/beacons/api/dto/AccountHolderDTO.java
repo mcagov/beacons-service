33028f7318793658b77dffabe92f7f51cc41b144
Matthew Carr <matt.carr@madetech.com>
2021-08-23 14:15:50 +0100

    Update integration test to create a legacy beacon before finding by id

--- src/main/java/uk/gov/mca/beacons/api/dto/AccountHolderDTO.java
+++ src/main/java/uk/gov/mca/beacons/api/dto/AccountHolderDTO.java
@@ -2,7 +2,6 @@ package uk.gov.mca.beacons.api.dto;
 
 import static uk.gov.mca.beacons.api.dto.AccountHolderDTO.Attributes;
 
-import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
--- src/test/java/uk/gov/mca/beacons/api/controllers/LegacyControllerIntegrationTest.java
+++ src/test/java/uk/gov/mca/beacons/api/controllers/LegacyControllerIntegrationTest.java
@@ -1,10 +1,15 @@
 package uk.gov.mca.beacons.api.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,8 +20,8 @@ class LegacyControllerIntegrationTest {
   private WebTestClient webTestClient;
 
   @Test
-  void shouldReturnHttp200ForFetchingALegacyBeaconById() {
-    final var legacyBeaconId = "c603c49d-257f-4445-a11c-52c3b7a90c36";
+  void shouldReturnHttp200ForFetchingALegacyBeaconById() throws Exception {
+    final var legacyBeaconId = createLegacyBeacon();
 
     webTestClient
       .get()
@@ -36,5 +41,29 @@ class LegacyControllerIntegrationTest {
       .exchange()
       .expectStatus()
       .isNotFound();
+  }
+
+  private String createLegacyBeacon() throws Exception {
+    final var createLegacyBeaconRequest = Files.readString(
+      Paths.get("src/test/resources/fixtures/createLegacyBeaconRequest.json")
+    );
+
+    final var createdAccountResponse = webTestClient
+      .post()
+      .uri("/migrate/legacy-beacon")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue(createLegacyBeaconRequest)
+      .exchange()
+      .expectStatus()
+      .isCreated()
+      .expectBody()
+      .returnResult()
+      .getResponseBody();
+
+    return new ObjectMapper()
+      .readValue(createdAccountResponse, ObjectNode.class)
+      .get("data")
+      .get("id")
+      .textValue();
   }
 }
