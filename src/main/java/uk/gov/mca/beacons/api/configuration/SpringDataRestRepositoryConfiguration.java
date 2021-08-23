7c14555f28534f62f12cf7065e086730edf3b897
Matthew Carr <matt.carr@madetech.com>
2021-08-23 09:52:00 +0100

    Add CORS mapping for spring data rest

--- src/main/java/uk/gov/mca/beacons/api/configuration/SpringDataRestRepositoryConfiguration.java
+++ src/main/java/uk/gov/mca/beacons/api/configuration/SpringDataRestRepositoryConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.mca.beacons.api.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,11 +11,18 @@ import uk.gov.mca.beacons.api.jpa.entities.BeaconSearchEntity;
 public class SpringDataRestRepositoryConfiguration
   implements RepositoryRestConfigurer {
 
+  @Value("${beacons.cors.allowedOrigins}")
+  private String[] allowedOrigins;
+
   @Override
   public void configureRepositoryRestConfiguration(
     RepositoryRestConfiguration config,
     CorsRegistry cors
   ) {
     config.exposeIdsFor(BeaconSearchEntity.class);
+
+    if (allowedOrigins != null && allowedOrigins.length > 0) {
+      cors.addMapping("/**").allowedMethods("*").allowedOrigins(allowedOrigins);
+    }
   }
 }
