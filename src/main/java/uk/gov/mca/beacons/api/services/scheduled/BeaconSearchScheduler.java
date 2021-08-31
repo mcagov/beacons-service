e16ee0423731a0d6c43939eae9e992909fb87a47
Matthew Carr <matt.carr@madetech.com>
2021-08-31 11:44:38 +0100

    Add migration to remove triggers for insert into beacon and related tables

--- src/main/java/uk/gov/mca/beacons/api/services/scheduled/BeaconSearchScheduler.java
+++ src/main/java/uk/gov/mca/beacons/api/services/scheduled/BeaconSearchScheduler.java
@@ -22,10 +22,11 @@ public class BeaconSearchScheduler {
 
   @Scheduled(fixedRateString = "${beacons.scheduled.beacon-search-view}")
   public void refreshView() {
-    log.info("Refreshing beacon search view");
+    log.debug("Refreshing beacon search view");
 
     final SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
       .withFunctionName("refresh_beacon_search_view_schedule");
     simpleJdbcCall.execute();
+    log.debug("Finished refreshing beacon search view");
   }
 }
---
+++ src/main/resources/db/migration/V1.33__Remove_Beacon_Table_Triggers.sql
@@ -0,0 +1,3 @@
+-- Drop beacon and related table triggers to increase insert speeds
+
+DROP FUNCTION refresh_beacon_search_view() CASCADE;
