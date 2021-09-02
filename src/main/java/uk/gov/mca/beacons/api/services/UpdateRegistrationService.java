57d250317c1e5fdee1115ec9873260415078ae33
Matthew Carr <matt.carr@madetech.com>
2021-09-02 17:03:15 +0100

    Remove unused import

--- src/main/java/uk/gov/mca/beacons/api/services/UpdateRegistrationService.java
+++ src/main/java/uk/gov/mca/beacons/api/services/UpdateRegistrationService.java
@@ -1,6 +1,5 @@
 package uk.gov.mca.beacons.api.services;
 
-import java.util.UUID;
 import org.springframework.stereotype.Service;
 import uk.gov.mca.beacons.api.dto.UpdateRegistrationRequest;
 import uk.gov.mca.beacons.api.jpa.entities.Beacon;
