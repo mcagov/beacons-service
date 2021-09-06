fcb4f9599c04d63d7b5b675f3ecdfcf924bd8ba4
Matthew Carr <matt.carr@madetech.com>
2021-09-06 11:07:38 +0100

    Update rest repository endpoint to only expose sorting options:

--- src/main/java/uk/gov/mca/beacons/api/controllers/BeaconSearchRestRepository.java
+++ src/main/java/uk/gov/mca/beacons/api/controllers/BeaconSearchRestRepository.java
@@ -3,6 +3,7 @@ package uk.gov.mca.beacons.api.controllers;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -55,6 +56,6 @@ interface BeaconSearchRestRepository
   Page<BeaconSearchEntity> findALlByAccountHolderIdAndEmail(
     @Param("email") String email,
     @Param("accountHolderId") UUID accountHolderId,
-    Pageable page
+    Sort sort
   );
 }
