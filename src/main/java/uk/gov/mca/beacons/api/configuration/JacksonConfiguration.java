428a5142e936b7d47f2d7767a9cf227dc8b9759c
Matthew Carr <matt.carr@madetech.com>
2021-09-09 15:04:07 +0100

    Remove unused import

--- src/main/java/uk/gov/mca/beacons/api/configuration/JacksonConfiguration.java
+++ src/main/java/uk/gov/mca/beacons/api/configuration/JacksonConfiguration.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.util.TimeZone;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
