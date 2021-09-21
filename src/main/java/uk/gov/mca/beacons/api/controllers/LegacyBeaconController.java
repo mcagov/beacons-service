1aac3887f6af489db5f691e0e6d37f57d89766fe
Olly Swanson <olly.swanson95@gmail.com>
2021-09-21 09:01:42 +0100

    Added getNotesByLegacyBeaconId to LegacyBeaconController

--- src/main/java/uk/gov/mca/beacons/api/controllers/LegacyBeaconController.java
+++ src/main/java/uk/gov/mca/beacons/api/controllers/LegacyBeaconController.java
@@ -1,6 +1,7 @@
 package uk.gov.mca.beacons.api.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.mca.beacons.api.domain.Note;
 import uk.gov.mca.beacons.api.dto.DeleteLegacyBeaconRequestDTO;
 import uk.gov.mca.beacons.api.dto.LegacyBeaconDTO;
+import uk.gov.mca.beacons.api.dto.NoteDTO;
 import uk.gov.mca.beacons.api.dto.WrapperDTO;
 import uk.gov.mca.beacons.api.exceptions.ResourceNotFoundException;
 import uk.gov.mca.beacons.api.mappers.LegacyBeaconMapper;
+import uk.gov.mca.beacons.api.mappers.NoteMapper;
 import uk.gov.mca.beacons.api.services.DeleteLegacyBeaconService;
 import uk.gov.mca.beacons.api.services.LegacyBeaconService;
+import uk.gov.mca.beacons.api.services.NoteService;
 
 @RestController
 @RequestMapping("/legacy-beacon")
@@ -28,16 +33,22 @@ public class LegacyBeaconController {
   private final LegacyBeaconService legacyBeaconService;
   private final LegacyBeaconMapper legacyBeaconMapper;
   private final DeleteLegacyBeaconService deleteLegacyBeaconService;
+  private final NoteService noteService;
+  private final NoteMapper noteMapper;
 
   @Autowired
   public LegacyBeaconController(
     LegacyBeaconService legacyBeaconService,
     LegacyBeaconMapper legacyBeaconMapper,
-    DeleteLegacyBeaconService deleteLegacyBeaconService
+    DeleteLegacyBeaconService deleteLegacyBeaconService,
+    NoteService noteService,
+    NoteMapper noteMapper
   ) {
     this.legacyBeaconService = legacyBeaconService;
     this.legacyBeaconMapper = legacyBeaconMapper;
     this.deleteLegacyBeaconService = deleteLegacyBeaconService;
+    this.noteService = noteService;
+    this.noteMapper = noteMapper;
   }
 
   @GetMapping(value = "/{uuid}")
@@ -47,6 +58,15 @@ public class LegacyBeaconController {
       .orElseThrow(ResourceNotFoundException::new);
 
     return legacyBeaconMapper.toWrapperDTO(legacyBeacon);
+  }
+
+  @GetMapping(value = "/{uuid}/notes")
+  public WrapperDTO<List<NoteDTO>> getNotesByLegacyBeaconId(
+    @PathVariable("uuid") UUID id
+  ) {
+    final List<Note> foundNotes = noteService.findAllByLegacyBeaconId(id);
+
+    return noteMapper.toOrderedWrapperDTO(foundNotes);
   }
 
   @PatchMapping(value = "/{uuid}/delete")
