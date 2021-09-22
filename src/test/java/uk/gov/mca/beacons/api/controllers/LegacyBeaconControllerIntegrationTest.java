8c7f3f8f81ae54b7116506b9461c2073ea90de5e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-22 15:40:23 +0100

    Fix integration test to hit correct beacon-search path

--- src/test/java/uk/gov/mca/beacons/api/controllers/LegacyBeaconControllerIntegrationTest.java
+++ src/test/java/uk/gov/mca/beacons/api/controllers/LegacyBeaconControllerIntegrationTest.java
@@ -24,162 +24,162 @@ import uk.gov.mca.beacons.api.gateways.UserGateway;
 @AutoConfigureWebTestClient
 class LegacyBeaconControllerIntegrationTest {
 
-  private User user;
+    private User user;
 
-  @Autowired
-  private WebTestClient webTestClient;
+    @Autowired
+    private WebTestClient webTestClient;
 
-  @MockBean
-  private UserGateway userGateway;
+    @MockBean
+    private UserGateway userGateway;
 
-  @BeforeEach
-  void init() {
-    final var userId = UUID.randomUUID();
+    @BeforeEach
+    void init() {
+        final var userId = UUID.randomUUID();
 
-    user =
-      AccountHolder
-        .builder()
-        .id(userId)
-        .email("beacon@beacons.com")
-        .fullName("Mr. Beacon")
-        .build();
-  }
-
-  @Test
-  void shouldReturnTheLegacyBeaconById() throws Exception {
-    final String legacyBeaconId = getLegacyBeaconId(createLegacyBeacon());
-    final var createLegacyBeaconResponse = Files.readString(
-      Paths.get("src/test/resources/fixtures/createLegacyBeaconResponse.json")
-    );
-
-    webTestClient
-      .get()
-      .uri("/legacy-beacon/" + legacyBeaconId)
-      .exchange()
-      .expectStatus()
-      .isOk()
-      .expectBody()
-      .json(createLegacyBeaconResponse)
-      .jsonPath("$.data.id")
-      .isEqualTo(legacyBeaconId);
-  }
-
-  @Test
-  void shouldReturnHttp404IfTheLegacyBeaconCannotBeFound() {
-    final var legacyBeaconId = UUID.randomUUID().toString();
-
-    webTestClient
-      .get()
-      .uri("/legacy-beacon/" + legacyBeaconId)
-      .exchange()
-      .expectStatus()
-      .isNotFound();
-  }
-
-  @Nested
-  class ClaimEvent {
+        user =
+                AccountHolder
+                        .builder()
+                        .id(userId)
+                        .email("beacon@beacons.com")
+                        .fullName("Mr. Beacon")
+                        .build();
+    }
 
     @Test
-    void whenTheUserHitsTheClaimEndpoint_aNewREgistrationIsCreatedWithTheNecessaryProperties()
-      throws Exception {
-      var legacyBeaconResponse = createLegacyBeacon();
-      var legacyBeaconId = getLegacyBeaconId(legacyBeaconResponse);
-      var legacyBeaconHexId = getLegacyBeaconHexId(legacyBeaconResponse);
+    void shouldReturnTheLegacyBeaconById() throws Exception {
+        final String legacyBeaconId = getLegacyBeaconId(createLegacyBeacon());
+        final var createLegacyBeaconResponse = Files.readString(
+                Paths.get("src/test/resources/fixtures/createLegacyBeaconResponse.json")
+        );
 
-      webTestClient
-        .post()
-        .uri("legacy-beacon/" + legacyBeaconId + "/claim")
-        .exchange()
-        .expectStatus()
-        .isOk();
-
-      webTestClient
-        .get()
-        .uri(
-          "beacon-search/find-all-by-account-holder-and-email?email=" +
-          user.getEmail() +
-          "&accountHolderId=" +
-          user.getId()
-        )
-        .exchange()
-        .expectStatus()
-        .isOk()
-        .expectBody()
-        .jsonPath("$.data[0].type")
-        .isEqualTo("beacon")
-        .jsonPath("$.data[0].attributes.hexId")
-        .isEqualTo(legacyBeaconHexId)
-        .jsonPath("$.data[0].attributes.status")
-        .isEqualTo("NEW");
+        webTestClient
+                .get()
+                .uri("/legacy-beacon/" + legacyBeaconId)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .json(createLegacyBeaconResponse)
+                .jsonPath("$.data.id")
+                .isEqualTo(legacyBeaconId);
     }
-  }
 
-  private WebTestClient.BodyContentSpec createLegacyBeacon() throws Exception {
-    final var createLegacyBeaconRequest = Files.readString(
-      Paths.get("src/test/resources/fixtures/createLegacyBeaconRequest.json")
-    );
+    @Test
+    void shouldReturnHttp404IfTheLegacyBeaconCannotBeFound() {
+        final var legacyBeaconId = UUID.randomUUID().toString();
 
-    return webTestClient
-      .post()
-      .uri("/migrate/legacy-beacon")
-      .contentType(MediaType.APPLICATION_JSON)
-      .bodyValue(createLegacyBeaconRequest)
-      .exchange()
-      .expectStatus()
-      .isCreated()
-      .expectBody();
-  }
+        webTestClient
+                .get()
+                .uri("/legacy-beacon/" + legacyBeaconId)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
 
-  private String getLegacyBeaconId(
-    WebTestClient.BodyContentSpec createLegacyBeaconResponse
-  ) throws Exception {
-    return new ObjectMapper()
-      .readValue(
-        createLegacyBeaconResponse.returnResult().getResponseBody(),
-        ObjectNode.class
-      )
-      .get("data")
-      .get("id")
-      .textValue();
-  }
+    private WebTestClient.BodyContentSpec createLegacyBeacon() throws Exception {
+        final var createLegacyBeaconRequest = Files.readString(
+                Paths.get("src/test/resources/fixtures/createLegacyBeaconRequest.json")
+        );
 
-  private String getLegacyBeaconHexId(
-    WebTestClient.BodyContentSpec createLegacyBeaconResponse
-  ) throws Exception {
-    return new ObjectMapper()
-      .readValue(
-        createLegacyBeaconResponse.returnResult().getResponseBody(),
-        ObjectNode.class
-      )
-      .get("data")
-      .get("attributes")
-      .get("beacon")
-      .get("hexId")
-      .textValue();
-  }
+        return webTestClient
+                .post()
+                .uri("/migrate/legacy-beacon")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(createLegacyBeaconRequest)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody();
+    }
 
-  private void deleteLegacyBeacon(
-    String legacyBeaconId,
-    String userId,
-    String reason
-  ) throws Exception {
-    final var deleteLegacyBeaconRequest = Files
-      .readString(
-        Paths.get("src/test/resources/fixtures/deleteLegacyBeaconRequest.json")
-      )
-      .replace("replace-with-test-legacy-beacon-id", legacyBeaconId)
-      .replace("replace-with-test-user-id", userId)
-      .replace("replace-with-test-reason", reason);
+    private String getLegacyBeaconId(
+            WebTestClient.BodyContentSpec createLegacyBeaconResponse
+    ) throws Exception {
+        return new ObjectMapper()
+                .readValue(
+                        createLegacyBeaconResponse.returnResult().getResponseBody(),
+                        ObjectNode.class
+                )
+                .get("data")
+                .get("id")
+                .textValue();
+    }
 
-    given(userGateway.getUserById(user.getId())).willReturn(user);
+    private String getLegacyBeaconHexId(
+            WebTestClient.BodyContentSpec createLegacyBeaconResponse
+    ) throws Exception {
+        return new ObjectMapper()
+                .readValue(
+                        createLegacyBeaconResponse.returnResult().getResponseBody(),
+                        ObjectNode.class
+                )
+                .get("data")
+                .get("attributes")
+                .get("beacon")
+                .get("hexId")
+                .textValue();
+    }
 
-    webTestClient
-      .patch()
-      .uri("/legacy-beacon/" + legacyBeaconId + "/delete")
-      .contentType(MediaType.APPLICATION_JSON)
-      .bodyValue(deleteLegacyBeaconRequest)
-      .exchange()
-      .expectStatus()
-      .isOk();
-  }
+    private void deleteLegacyBeacon(
+            String legacyBeaconId,
+            String userId,
+            String reason
+    ) throws Exception {
+        final var deleteLegacyBeaconRequest = Files
+                .readString(
+                        Paths.get("src/test/resources/fixtures/deleteLegacyBeaconRequest.json")
+                )
+                .replace("replace-with-test-legacy-beacon-id", legacyBeaconId)
+                .replace("replace-with-test-user-id", userId)
+                .replace("replace-with-test-reason", reason);
+
+        given(userGateway.getUserById(user.getId())).willReturn(user);
+
+        webTestClient
+                .patch()
+                .uri("/legacy-beacon/" + legacyBeaconId + "/delete")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(deleteLegacyBeaconRequest)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @Nested
+    class ClaimEvent {
+
+        @Test
+        void whenTheUserHitsTheClaimEndpoint_aNewRegistrationIsCreatedWithTheNecessaryProperties()
+                throws Exception {
+            var legacyBeaconResponse = createLegacyBeacon();
+            var legacyBeaconId = getLegacyBeaconId(legacyBeaconResponse);
+            var legacyBeaconHexId = getLegacyBeaconHexId(legacyBeaconResponse);
+
+            webTestClient
+                    .post()
+                    .uri("legacy-beacon/" + legacyBeaconId + "/claim")
+                    .exchange()
+                    .expectStatus()
+                    .isOk();
+
+            webTestClient
+                    .get()
+                    .uri(
+                            "beacon-search/search/find-all-by-account-holder-and-email?email=" +
+                                    user.getEmail() +
+                                    "&accountHolderId=" +
+                                    user.getId().toString()
+                    )
+                    .exchange()
+                    .expectStatus()
+                    .isOk()
+                    .expectBody()
+                    .jsonPath("$.data[0].type")
+                    .isEqualTo("beacon")
+                    .jsonPath("$.data[0].attributes.hexId")
+                    .isEqualTo(legacyBeaconHexId)
+                    .jsonPath("$.data[0].attributes.status")
+                    .isEqualTo("NEW");
+        }
+    }
 }
