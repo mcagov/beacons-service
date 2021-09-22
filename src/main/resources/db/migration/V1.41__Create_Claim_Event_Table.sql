a9bf41b000d48dcd5616f43d917e6aa58ea3489f
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-22 16:55:25 +0100

    Migration - Create claim_event table

---
+++ src/main/resources/db/migration/V1.41__Create_Claim_Event_Table.sql
@@ -0,0 +1,12 @@
+-- Create a claim_event table for persisting LegacyBeaconClaimEvent objects
+CREATE TYPE claim AS ENUM ('claim', 'reject');
+
+CREATE TABLE IF NOT EXISTS claim_event
+(
+    id               uuid PRIMARY KEY,
+    type             claim                              NOT NULL,
+    legacy_beacon_id uuid REFERENCES legacy_beacon (id) NOT NULL,
+    account_holder   uuid REFERENCES person (id)        NOT NULL,
+    date_time        TIMESTAMP WITH TIME ZONE           NOT NULL,
+    reason           varchar(500)
+)
