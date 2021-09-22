190fc1c82fa7975a5a7abbc61372357a215ef566
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-22 14:51:31 +0100

    Define EventGateway abstraction

--- src/main/java/uk/gov/mca/beacons/api/gateways/EventGateway.java
+++ src/main/java/uk/gov/mca/beacons/api/gateways/EventGateway.java
@@ -1,5 +1,7 @@
 package uk.gov.mca.beacons.api.gateways;
 
-interface EventGateway {
-  public void save(Event event);
+import uk.gov.mca.beacons.api.domain.events.Event;
+
+public interface EventGateway {
+    void save(Event event);
 }
