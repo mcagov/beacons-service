5b4a7fc5c87d49f6b65b744dee6ff54b6d917430
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-24 14:08:34 +0100

    More WIP

--- src/main/java/uk/gov/mca/beacons/api/gateways/LegacyBeaconGatewayImpl.java
+++ src/main/java/uk/gov/mca/beacons/api/gateways/LegacyBeaconGatewayImpl.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.mca.beacons.api.domain.BeaconStatus;
@@ -20,71 +19,71 @@ import uk.gov.mca.beacons.api.mappers.LegacyBeaconMapper;
 @Slf4j
 public class LegacyBeaconGatewayImpl implements LegacyBeaconGateway {
 
-  private final LegacyBeaconJpaRepository legacyBeaconJpaRepository;
-  private final LegacyBeaconMapper legacyBeaconMapper;
-  private final JdbcTemplate jdbcTemplate;
+    private final LegacyBeaconJpaRepository legacyBeaconJpaRepository;
+    private final LegacyBeaconMapper legacyBeaconMapper;
+    private final JdbcTemplate jdbcTemplate;
 
-  @Autowired
-  public LegacyBeaconGatewayImpl(
-    LegacyBeaconJpaRepository legacyBeaconJpaRepository,
-    LegacyBeaconMapper legacyBeaconMapper,
-    JdbcTemplate jdbcTemplate
-  ) {
-    this.legacyBeaconJpaRepository = legacyBeaconJpaRepository;
-    this.legacyBeaconMapper = legacyBeaconMapper;
-    this.jdbcTemplate = jdbcTemplate;
-  }
+    @Autowired
+    public LegacyBeaconGatewayImpl(
+            LegacyBeaconJpaRepository legacyBeaconJpaRepository,
+            LegacyBeaconMapper legacyBeaconMapper,
+            JdbcTemplate jdbcTemplate
+    ) {
+        this.legacyBeaconJpaRepository = legacyBeaconJpaRepository;
+        this.legacyBeaconMapper = legacyBeaconMapper;
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-  @Override
-  public LegacyBeacon save(LegacyBeacon beacon) {
-    final var legacyBeaconEntity = legacyBeaconMapper.toJpaEntity(beacon);
-    legacyBeaconEntity.setBeaconStatus(BeaconStatus.MIGRATED);
+    @Override
+    public LegacyBeacon save(LegacyBeacon beacon) {
+        final var legacyBeaconEntity = legacyBeaconMapper.toJpaEntity(beacon);
+        legacyBeaconEntity.setBeaconStatus(BeaconStatus.MIGRATED);
 
-    log.info(
-      "Saving beacon record with PK {}",
-      beacon.getBeacon().get("pkBeaconId")
-    );
-    return legacyBeaconMapper.fromJpaEntity(
-      legacyBeaconJpaRepository.save(legacyBeaconEntity)
-    );
-  }
+        log.info(
+                "Saving beacon record with PK {}",
+                beacon.getBeacon().get("pkBeaconId")
+        );
+        return legacyBeaconMapper.fromJpaEntity(
+                legacyBeaconJpaRepository.save(legacyBeaconEntity)
+        );
+    }
 
-  @Override
-  public void delete(UUID id) {
-    final LegacyBeaconEntity legacyBeacon = legacyBeaconJpaRepository
-      .findById(id)
-      .orElseThrow(ResourceNotFoundException::new);
+    @Override
+    public void delete(UUID id) {
+        final LegacyBeaconEntity legacyBeacon = legacyBeaconJpaRepository
+                .findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
 
-    legacyBeacon.setBeaconStatus(BeaconStatus.DELETED);
-    legacyBeaconJpaRepository.save(legacyBeacon);
-  }
+        legacyBeacon.setBeaconStatus(BeaconStatus.DELETED);
+        legacyBeaconJpaRepository.save(legacyBeacon);
+    }
 
-  @Override
-  public void deleteAll() {
-    jdbcTemplate.execute("DELETE FROM legacy_beacon");
-  }
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.execute("DELETE FROM legacy_beacon");
+    }
 
-  @Override
-  public Optional<LegacyBeacon> findById(UUID id) {
-    return legacyBeaconJpaRepository
-      .findById(id)
-      .map(legacyBeaconMapper::fromJpaEntity);
-  }
+    @Override
+    public Optional<LegacyBeacon> findById(UUID id) {
+        return legacyBeaconJpaRepository
+                .findById(id)
+                .map(legacyBeaconMapper::fromJpaEntity);
+    }
 
-  @Override
-  public Optional<LegacyBeacon> actualFindById(UUID id) {
-    final SqlParameterSource legacyBeaconParamMap = new MapSqlParameterSource()
-      .addValue("id", id);
-
-    LegacyBeacon legacyBeacon = jdbcTemplate.queryForObject(
-      "SELECT " +
-      "legacy_beacon.hex_id, " +
-      "legacy_beacon.owner_email, " +
-      "legacy_beacon.created_date, " +
-      "legacy_beacon.last_modified_date, " +
-      "legacy_beacon.owner_name, " +
-      "legacy_beacon.use_activities, "
-    );
-    return Optional.of(legacyBeacon);
-  }
+//  @Override
+//  public Optional<LegacyBeacon> actualFindById(UUID id) {
+//    final SqlParameterSource legacyBeaconParamMap = new MapSqlParameterSource()
+//      .addValue("id", id);
+//
+//    LegacyBeacon legacyBeacon = jdbcTemplate.queryForObject(
+//      "SELECT " +
+//      "legacy_beacon.hex_id, " +
+//      "legacy_beacon.owner_email, " +
+//      "legacy_beacon.created_date, " +
+//      "legacy_beacon.last_modified_date, " +
+//      "legacy_beacon.owner_name, " +
+//      "legacy_beacon.use_activities, "
+//    );
+//    return Optional.of(legacyBeacon);
+//  }
 }
--- src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
+++ src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
@@ -13,7 +13,6 @@ import uk.gov.mca.beacons.api.exceptions.BeaconsValidationException;
 import uk.gov.mca.beacons.api.gateways.AccountHolderGateway;
 import uk.gov.mca.beacons.api.gateways.EventGateway;
 import uk.gov.mca.beacons.api.gateways.LegacyBeaconGateway;
-import uk.gov.mca.beacons.api.jpa.entities.Beacon;
 import uk.gov.mca.beacons.api.mappers.LegacyBeaconMapper;
 import uk.gov.mca.beacons.api.services.validation.LegacyBeaconValidator;
 
@@ -21,64 +20,64 @@ import uk.gov.mca.beacons.api.services.validation.LegacyBeaconValidator;
 @Transactional
 public class LegacyBeaconService {
 
-  private final LegacyBeaconGateway legacyBeaconGateway;
-  private final EventGateway eventGateway;
-  private final AccountHolderGateway accountHolderGateway;
-  private final LegacyBeaconValidator legacyBeaconValidator;
-  private final LegacyBeaconMapper legacyBeaconMapper;
-  private final CreateRegistrationService createRegistrationService;
+    private final LegacyBeaconGateway legacyBeaconGateway;
+    private final EventGateway eventGateway;
+    private final AccountHolderGateway accountHolderGateway;
+    private final LegacyBeaconValidator legacyBeaconValidator;
+    private final LegacyBeaconMapper legacyBeaconMapper;
+    private final CreateRegistrationService createRegistrationService;
 
-  @Autowired
-  public LegacyBeaconService(
-    LegacyBeaconGateway legacyBeaconGateway,
-    EventGateway eventGateway,
-    AccountHolderGateway accountHolderGateway,
-    LegacyBeaconValidator legacyBeaconValidator,
-    LegacyBeaconMapper legacyBeaconMapper,
-    CreateRegistrationService createRegistrationService
-  ) {
-    this.legacyBeaconGateway = legacyBeaconGateway;
-    this.eventGateway = eventGateway;
-    this.accountHolderGateway = accountHolderGateway;
-    this.legacyBeaconValidator = legacyBeaconValidator;
-    this.legacyBeaconMapper = legacyBeaconMapper;
-    this.createRegistrationService = createRegistrationService;
-  }
-
-  public LegacyBeacon create(LegacyBeacon beacon) {
-    validate(beacon);
-    return legacyBeaconGateway.save(beacon);
-  }
-
-  private void validate(LegacyBeacon beacon) {
-    final var errors = new BeanPropertyBindingResult(beacon, "legacyBeacon");
-    legacyBeaconValidator.validate(beacon, errors);
-
-    if (errors.hasFieldErrors()) {
-      throw new BeaconsValidationException(errors);
+    @Autowired
+    public LegacyBeaconService(
+            LegacyBeaconGateway legacyBeaconGateway,
+            EventGateway eventGateway,
+            AccountHolderGateway accountHolderGateway,
+            LegacyBeaconValidator legacyBeaconValidator,
+            LegacyBeaconMapper legacyBeaconMapper,
+            CreateRegistrationService createRegistrationService
+    ) {
+        this.legacyBeaconGateway = legacyBeaconGateway;
+        this.eventGateway = eventGateway;
+        this.accountHolderGateway = accountHolderGateway;
+        this.legacyBeaconValidator = legacyBeaconValidator;
+        this.legacyBeaconMapper = legacyBeaconMapper;
+        this.createRegistrationService = createRegistrationService;
     }
-  }
 
-  public void deleteAll() {
-    legacyBeaconGateway.deleteAll();
-  }
+    public LegacyBeacon create(LegacyBeacon beacon) {
+        validate(beacon);
+        return legacyBeaconGateway.save(beacon);
+    }
 
-  public Optional<LegacyBeacon> findById(UUID id) {
-    return legacyBeaconGateway.findById(id);
-  }
+    private void validate(LegacyBeacon beacon) {
+        final var errors = new BeanPropertyBindingResult(beacon, "legacyBeacon");
+        legacyBeaconValidator.validate(beacon, errors);
 
-  public void claim(LegacyBeacon legacyBeacon) {
-    var accountHolder = accountHolderGateway.getByEmail(
-      legacyBeacon.getAssociatedEmailAddress()
-    );
-    eventGateway.save(
-      new LegacyBeaconClaimEvent(
-        legacyBeacon,
-        accountHolder,
-        OffsetDateTime.now()
-      )
-    );
-    Beacon beacon = legacyBeaconMapper.toBeacon(legacyBeacon);
-    createRegistrationService.register(beacon);
-  }
+        if (errors.hasFieldErrors()) {
+            throw new BeaconsValidationException(errors);
+        }
+    }
+
+    public void deleteAll() {
+        legacyBeaconGateway.deleteAll();
+    }
+
+    public Optional<LegacyBeacon> findById(UUID id) {
+        return legacyBeaconGateway.findById(id);
+    }
+
+    public void claim(LegacyBeacon legacyBeacon) {
+        var accountHolder = accountHolderGateway.getByEmail(
+                legacyBeacon.getAssociatedEmailAddress()
+        );
+        eventGateway.save(
+                new LegacyBeaconClaimEvent(
+                        legacyBeacon,
+                        accountHolder,
+                        OffsetDateTime.now()
+                )
+        );
+        // Beacon beacon = legacyBeaconMapper.toBeacon(legacyBeacon);
+        createRegistrationService.register(beacon);
+    }
 }
