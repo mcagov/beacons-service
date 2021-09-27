5c1f98264ed322313df6991799ab87c8c471bc88
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-27 17:17:26 +0100

    Add claim_event filtering to beacon_search_view

--- src/main/resources/db/migration/V1.40__Create_Legacy_Beacon_Event_Table.sql
+++ src/main/resources/db/migration/V1.40__Create_Legacy_Beacon_Event_Table.sql
@@ -3,9 +3,9 @@ CREATE TYPE claim_event_type AS ENUM ('claim', 'reject');
 CREATE TABLE IF NOT EXISTS claim_event
 (
     id                UUID PRIMARY KEY,
-    type              claim_event_type                    NOT NULL,
-    legacy_beacon_id  UUID REFERENCES legacy_beacon (id)  NOT NULL,
-    account_holder_id UUID REFERENCES account_holder (id) NOT NULL,
-    date_time         TIMESTAMP WITH TIME ZONE            NOT NULL,
+    type              claim_event_type                                      NOT NULL,
+    legacy_beacon_id  UUID REFERENCES legacy_beacon (id) ON DELETE CASCADE  NOT NULL,
+    account_holder_id UUID REFERENCES account_holder (id) ON DELETE CASCADE NOT NULL,
+    date_time         TIMESTAMP WITH TIME ZONE                              NOT NULL,
     reason            VARCHAR(500)
 )
---
+++ src/main/resources/db/migration/V1.41__Add_Claim_Event_Filtering_To_Beacon_Search_View.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE VIEW beacon_search AS
+SELECT id,
+       created_date,
+       last_modified_date,
+       (CASE
+            WHEN (SELECT COUNT(*) FROM claim_event WHERE claim_event.legacy_beacon_id = legacy_beacon.id) = 0
+                THEN 'MIGRATED'
+            ELSE 'DELETED' END) AS beacon_status,
+       hex_id,
+       owner_name,
+       owner_email,
+       NULL                     as account_holder_id,
+       use_activities
+FROM legacy_beacon
+UNION
+SELECT id,
+       created_date,
+       last_modified_date,
+       beacon_status,
+       hex_id,
+       (SELECT full_name FROM person WHERE person.person_type = 'OWNER' AND person.beacon_id = beacon.id) AS owner_name,
+       (SELECT email
+        FROM person
+        WHERE person.person_type = 'OWNER'
+          AND person.beacon_id = beacon.id)                                                               AS owner_email,
+       (SELECT id
+        FROM account_holder
+        WHERE account_holder.id = beacon.account_holder_id)                                               AS account_holder_id,
+       (SELECT REPLACE(string_agg(activity, ', '), '_', ' ')
+        FROM beacon_use
+        WHERE beacon_use.beacon_id = beacon.id)                                                           AS use_activities
+FROM beacon;
