11b95d3221a4108a87b49f62d56f168968c5781f
Olly Swanson <olly.swanson95@gmail.com>
2021-09-27 13:54:46 +0100

    Created EventGateway and added save method stub for LegacyBeaconClaimEvent

---
+++ src/main/java/uk/gov/mca/beacons/api/gateways/EventGateway.java
@@ -0,0 +1,7 @@
+package uk.gov.mca.beacons.api.gateways;
+
+import uk.gov.mca.beacons.api.domain.events.LegacyBeaconClaimEvent;
+
+public interface EventGateway {
+  void save(LegacyBeaconClaimEvent legacyBeaconClaimEvent) throws Exception;
+}
