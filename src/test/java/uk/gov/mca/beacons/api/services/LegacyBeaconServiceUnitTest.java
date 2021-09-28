b0555e35a42a5c511817ee6cd8ef736c831e3839
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-28 11:12:06 +0100

    Refactor for readability and DRY

--- src/test/java/uk/gov/mca/beacons/api/services/LegacyBeaconServiceUnitTest.java
+++ src/test/java/uk/gov/mca/beacons/api/services/LegacyBeaconServiceUnitTest.java
@@ -16,7 +16,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.mca.beacons.api.domain.AccountHolder;
 import uk.gov.mca.beacons.api.domain.LegacyBeacon;
 import uk.gov.mca.beacons.api.domain.events.LegacyBeaconClaimEvent;
-import uk.gov.mca.beacons.api.gateways.AccountHolderGateway;
 import uk.gov.mca.beacons.api.gateways.EventGateway;
 import uk.gov.mca.beacons.api.gateways.LegacyBeaconGateway;
 import uk.gov.mca.beacons.api.jpa.entities.Beacon;
@@ -34,119 +33,99 @@ public class LegacyBeaconServiceUnitTest {
   private AccountHolderService accountHolderService;
 
   @Mock
-  private AccountHolderGateway accountHolderGateway;
-
-  @Mock
   private EventGateway eventGateway;
 
   @Test
   void whenTheEmailAndHexIdAreTheSameInBothBeaconAndLegacyBeacon_thenReturnAMatch() {
-    // Setup Beacon
-    String hexId = "1D1234123412345";
-    String email = "test@test.com";
     UUID accountHolderId = UUID.randomUUID();
-    Beacon beacon = new Beacon();
-    beacon.setHexId(hexId);
-    beacon.setAccountHolderId(accountHolderId);
-
-    // Setup LegacyBeacon
-    Map<String, Object> beaconMap = new HashMap<>();
-    Map<String, Object> ownerMap = new HashMap<>();
-    beaconMap.put("hexId", hexId);
-    ownerMap.put("email", email);
-    List<LegacyBeacon> foundLegacyBeacons = List.of(
-      LegacyBeacon.builder().beacon(beaconMap).owner(ownerMap).build()
-    );
-    given(legacyBeaconGateway.findAllByHexIdAndEmail(hexId, email))
-      .willReturn(foundLegacyBeacons);
-
-    // Setup AccountHolder
+    String accountHolderEmail = "test@test.com";
     AccountHolder accountHolder = AccountHolder
       .builder()
-      .email(email)
+      .email(accountHolderEmail)
       .id(accountHolderId)
       .build();
     given(accountHolderService.getById(accountHolderId))
       .willReturn(accountHolder);
 
-    // Act
+    String hexId = "1D1234123412345";
+    Beacon beacon = new Beacon();
+    beacon.setHexId(hexId);
+    beacon.setAccountHolderId(accountHolderId);
+
+    String emailAssociatedWithLegacyBeacon = accountHolderEmail;
+    List<LegacyBeacon> foundLegacyBeacons = List.of(
+      LegacyBeacon
+        .builder()
+        .beacon(Map.of("hexId", hexId))
+        .owner(Map.of("email", emailAssociatedWithLegacyBeacon))
+        .build()
+    );
+    given(
+      legacyBeaconGateway.findAllByHexIdAndEmail(
+        hexId,
+        emailAssociatedWithLegacyBeacon
+      )
+    )
+      .willReturn(foundLegacyBeacons);
+
     List<LegacyBeacon> matchingLegacyBeacons = legacyBeaconService
       .findMatchingLegacyBeacons(beacon)
-      .get();
+      .orElseThrow();
 
-    // Assert
     assertThat(matchingLegacyBeacons.get(0), is(foundLegacyBeacons.get(0)));
   }
 
   @Test
   void whenTheEmailIsTheSameButTheHexIdIsNot_thenDontMatch() {
-    // Setup Beacon
-    String hexId = "does not match";
-    String email = "test@test.com";
+    String accountHolderEmail = "test@test.com";
     UUID accountHolderId = UUID.randomUUID();
-    Beacon beacon = new Beacon();
-    beacon.setHexId(hexId);
-    beacon.setAccountHolderId(accountHolderId);
-
-    // Setup LegacyBeacon
-    Map<String, Object> beaconMap = new HashMap<>();
-    Map<String, Object> ownerMap = new HashMap<>();
-    beaconMap.put("hexId", "different hexId");
-    ownerMap.put("email", email);
-    given(legacyBeaconGateway.findAllByHexIdAndEmail(hexId, email))
-      .willReturn(new ArrayList<>());
-
-    // Setup AccountHolder
     AccountHolder accountHolder = AccountHolder
       .builder()
-      .email(email)
+      .email(accountHolderEmail)
       .id(accountHolderId)
       .build();
     given(accountHolderService.getById(accountHolderId))
       .willReturn(accountHolder);
 
-    // Act
+    String hexId = "does not match";
+    Beacon beacon = new Beacon();
+    beacon.setHexId(hexId);
+    beacon.setAccountHolderId(accountHolderId);
+
+    given(legacyBeaconGateway.findAllByHexIdAndEmail(hexId, accountHolderEmail))
+      .willReturn(new ArrayList<>());
+
     List<LegacyBeacon> matchingLegacyBeacons = legacyBeaconService
       .findMatchingLegacyBeacons(beacon)
-      .get();
+      .orElseThrow();
 
-    // Assert
     assertThat(matchingLegacyBeacons.size(), is(0));
   }
 
   @Test
   void whenTheHexIdIsTheSameButTheEmailIsNot_thenDontMatch() {
-    // Setup Beacon
-    String hexId = "1D1234123412345";
-    String email = "doesnot@match.com";
+    String accountHolderEmail = "doesNotMatch@AnyLegacyBeacons.com";
     UUID accountHolderId = UUID.randomUUID();
-    Beacon beacon = new Beacon();
-    beacon.setHexId(hexId);
-    beacon.setAccountHolderId(accountHolderId);
-
-    // Setup LegacyBeacon
-    Map<String, Object> beaconMap = new HashMap<>();
-    Map<String, Object> ownerMap = new HashMap<>();
-    beaconMap.put("hexId", hexId);
-    ownerMap.put("email", "isnotamatchingemail@address.com");
-    given(legacyBeaconGateway.findAllByHexIdAndEmail(hexId, email))
-      .willReturn(new ArrayList<>());
-
-    // Setup AccountHolder
     AccountHolder accountHolder = AccountHolder
       .builder()
-      .email(email)
+      .email(accountHolderEmail)
       .id(accountHolderId)
       .build();
     given(accountHolderService.getById(accountHolderId))
       .willReturn(accountHolder);
 
-    // Act
+    String hexId = "1D1234123412345";
+    Beacon beacon = new Beacon();
+    beacon.setHexId(hexId);
+    beacon.setAccountHolderId(accountHolderId);
+
+    given(legacyBeaconGateway.findAllByHexIdAndEmail(hexId, accountHolderEmail))
+      .willReturn(new ArrayList<>());
+
     List<LegacyBeacon> matchingLegacyBeacons = legacyBeaconService
       .findMatchingLegacyBeacons(beacon)
-      .get();
+      .orElseThrow();
 
-    // Assert
     assertThat(matchingLegacyBeacons.size(), is(0));
   }
 
