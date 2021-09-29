906cc80df4c38fe8df3a38323e05c3d78c28c526
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-29 16:51:12 +0100

    Delete EventGateway + implementation
    We identified that Event is a member of the aggregate, so should not have its own object lifecycle.
    
    Events will be retrieved/saved inside the Gateway for their aggregate, e.g. LegacyBeaconGateway.

--- src/main/java/uk/gov/mca/beacons/api/gateways/EventGateway.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.mca.beacons.api.gateways;
-
-import uk.gov.mca.beacons.api.domain.events.LegacyBeaconClaimEvent;
-
-public interface EventGateway {
-  void save(LegacyBeaconClaimEvent legacyBeaconClaimEvent) throws Exception;
-}
