25207d19d09256ab9f5810b819048e94bed8ba74
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-29 19:18:33 +0100

    Add @SuperBuilder to Event classes to simplify construction

--- src/main/java/uk/gov/mca/beacons/api/domain/events/LegacyBeaconClaimEvent.java
+++ src/main/java/uk/gov/mca/beacons/api/domain/events/LegacyBeaconClaimEvent.java
@@ -4,10 +4,12 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import uk.gov.mca.beacons.api.domain.LegacyBeacon;
 
 @Getter
 @Setter
+@SuperBuilder
 public class LegacyBeaconClaimEvent extends LegacyBeaconEvent {
 
   private UUID accountHolderId;
--- src/main/java/uk/gov/mca/beacons/api/domain/events/LegacyBeaconEvent.java
+++ src/main/java/uk/gov/mca/beacons/api/domain/events/LegacyBeaconEvent.java
@@ -4,10 +4,12 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import uk.gov.mca.beacons.api.domain.LegacyBeacon;
 
 @Getter
 @Setter
+@SuperBuilder
 public abstract class LegacyBeaconEvent extends Event {
 
   protected LegacyBeacon legacyBeacon;
