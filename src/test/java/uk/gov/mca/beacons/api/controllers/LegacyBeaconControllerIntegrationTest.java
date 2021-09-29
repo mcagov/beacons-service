04af003c3b04a72bcc90f86b4ba74531402af6b5
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-29 11:05:04 +0100

    Failing integration test: Claimed LegacyBeacons do not flag as withdrawn

--- src/test/java/uk/gov/mca/beacons/api/controllers/LegacyBeaconControllerIntegrationTest.java
+++ src/test/java/uk/gov/mca/beacons/api/controllers/LegacyBeaconControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package uk.gov.mca.beacons.api.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -69,5 +68,57 @@ class LegacyBeaconControllerIntegrationTest {
       .get("data")
       .get("id")
       .textValue();
+  }
+
+  @Test
+  void shouldShowClaimedLegacyBeaconsAsWithdrawnWithReason() throws Exception {
+    // Arrange
+    TestSeeder testSeeder = new TestSeeder(webTestClient);
+    final var email = UUID.randomUUID() + "@test.com";
+    final var legacyBeaconHexId = "1D0EA08C52FFBFF";
+    final var createAccountResponseBody = testSeeder.seedAccountHolderWithEmail(
+      email
+    );
+    final var accountHolderId = testSeeder.getAccountHolderId(
+      createAccountResponseBody
+    );
+    WebTestClient.BodyContentSpec seededLegacyBeaconResponseBody = testSeeder.seedLegacyBeaconWithEmailAndHexId(
+      email,
+      legacyBeaconHexId
+    );
+    String legacyBeaconId = testSeeder.getLegacyBeaconId(
+      seededLegacyBeaconResponseBody
+    );
+
+    final Object requestBody = testSeeder
+      .registrationFixtureToMap(
+        testSeeder
+          .readFile("src/test/resources/fixtures/registrations.json")
+          .replace("replace-with-test-account-holder-id", accountHolderId)
+      )
+      .get(
+        RegistrationsControllerIntegrationTest.RegistrationUseCase.SINGLE_BEACON
+      );
+
+    // assertions
+    webTestClient
+      .post()
+      .uri("/registrations/register")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue(requestBody)
+      .exchange()
+      .expectStatus()
+      .isCreated();
+
+    // Act
+    webTestClient
+      .get()
+      .uri("/legacy-beacon/" + legacyBeaconId)
+      .exchange()
+      .expectStatus()
+      .isOk()
+      .expectBody()
+      .jsonPath("$.data.attributes.beacon.isWithdrawn")
+      .isEqualTo(true);
   }
 }
