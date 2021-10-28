4aef817034b684b80d653212e7469cf9ec5b4437
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-10-28 18:13:44 +0100

    Use relative URI for API + set proxy in package.json for local dev

--- src/main/backoffice/src/config.ts
+++ src/main/backoffice/src/config.ts
@@ -1,5 +1,5 @@
 export const applicationConfig = {
-  apiUrl: "http://localhost:8080/spring-api",
+  apiUrl: "../spring-api",
   azureADClientId: "8fb0f9ea-6351-4251-bcca-85e298bda8e7",
   azureADTenantId: "3fd408b5-82e6-4dc0-a36c-6e2aa815db3e",
   azureADAPIScopeURI: `api://8fb0f9ea-6351-4251-bcca-85e298bda8e7/access_as_user`,
