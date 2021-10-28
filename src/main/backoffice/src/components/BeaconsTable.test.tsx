58667afbe1f2be844119bd683371123b75a579ea
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-10-28 09:05:22 +0100

    Wrap component in <BrowserRouter> so <Link> works in tests
    https://stackoverflow.com/a/54519524

--- src/main/backoffice/src/components/BeaconsTable.test.tsx
+++ src/main/backoffice/src/components/BeaconsTable.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { IBeaconsGateway } from "gateways/beacons/IBeaconsGateway";
+import { BrowserRouter } from "react-router-dom";
 import { beaconSearchResultFixture } from "../fixtures/beaconSearchResult.fixture";
 import { BeaconsTable } from "./BeaconsTable";
 
@@ -17,14 +18,12 @@ describe("<BeaconsTable>", () => {
 
   const numberOfMockedBeacons = beaconSearchResultFixture.page.totalElements;
 
-  it("renders a table", async () => {
-    render(<BeaconsTable beaconsGateway={beaconsGatewayDouble} />);
-
-    expect((await screen.findAllByRole("table")).length).toBeGreaterThan(0);
-  });
-
   it("queries the injected gateway for beacon data", async () => {
-    render(<BeaconsTable beaconsGateway={beaconsGatewayDouble} />);
+    render(
+      <BrowserRouter>
+        <BeaconsTable beaconsGateway={beaconsGatewayDouble} />
+      </BrowserRouter>
+    );
 
     await waitFor(() => {
       expect(beaconsGatewayDouble.getAllBeacons).toHaveBeenCalled();
@@ -32,26 +31,24 @@ describe("<BeaconsTable>", () => {
   });
 
   it("displays the returned beacon data in the table", async () => {
-    render(<BeaconsTable beaconsGateway={beaconsGatewayDouble} />);
+    render(
+      <BrowserRouter>
+        <BeaconsTable beaconsGateway={beaconsGatewayDouble} />
+      </BrowserRouter>
+    );
 
     expect(await screen.findByText("Hex me difficultly")).toBeVisible();
   });
 
   it(`displays ${numberOfMockedBeacons} rows when ${numberOfMockedBeacons} beacons are returned from the gateway`, async () => {
-    render(<BeaconsTable beaconsGateway={beaconsGatewayDouble} />);
+    render(
+      <BrowserRouter>
+        <BeaconsTable beaconsGateway={beaconsGatewayDouble} />
+      </BrowserRouter>
+    );
 
     expect(await screen.findAllByTestId("beacons-table-row")).toHaveLength(
       numberOfMockedBeacons
     );
   });
-
-  // it("can click on the hex ID to see more details about the beacon", async () => {
-  //   render(<BeaconsTable beaconsGateway={beaconsGatewayDouble} />);
-
-  //   const hexIdField = await screen.findByText("Hex me");
-
-  //   expect(hexIdField.getAttribute("href")).toBe(
-  //     "/#/beacons/97b306aa-cbd0-4f09-aa24-2d876b983efb"
-  //   );
-  // });
 });
