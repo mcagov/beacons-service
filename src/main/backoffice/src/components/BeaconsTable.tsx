bd96e04eb87026a0a960252d16395b15554683ad
Olly Swanson <olly.swanson95@gmail.com>
2021-11-01 21:27:03 +0000

    Add extra columns and update BeaconSearchResult interface

--- src/main/backoffice/src/components/BeaconsTable.tsx
+++ src/main/backoffice/src/components/BeaconsTable.tsx
@@ -17,11 +17,16 @@ import {
   ViewColumn,
 } from "@material-ui/icons";
 import { IBeaconsGateway } from "gateways/beacons/IBeaconsGateway";
-import MaterialTable, { Icons, MTableBodyRow } from "material-table";
+import MaterialTable, {
+  Column,
+  Icons,
+  MTableBodyRow,
+} from "@material-table/core";
 import React, { forwardRef, FunctionComponent, useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Placeholders } from "utils/writingStyle";
 import { IBeaconSearchResultData } from "../entities/IBeaconSearchResult";
+import { replaceNone } from "../lib/legacyData/replaceNone";
 
 interface IBeaconsTableProps {
   beaconsGateway: IBeaconsGateway;
@@ -35,46 +40,39 @@ interface BeaconTableListRow {
   lastModifiedDate: string;
   beaconStatus: string;
   beaconType: string;
+  cospasSarsatNumber: string;
+  manufacturerSerialNumber: string;
+  serialNumber: string;
 }
 
-export const BeaconsTable: FunctionComponent<IBeaconsTableProps> = ({
-  beaconsGateway,
-}): JSX.Element => {
-  const tableIcons: Icons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => (
-      <ChevronRight {...props} ref={ref} />
-    )),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => (
-      <ChevronLeft {...props} ref={ref} />
-    )),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => (
-      <ArrowDownward {...props} ref={ref} />
-    )),
-    ThirdStateCheck: forwardRef((props, ref) => (
-      <Remove {...props} ref={ref} />
-    )),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-  };
+const tableIcons: Icons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  return (
-    <MaterialTable
-      icons={tableIcons}
-      isLoading={isLoading}
-      columns={[
+export const BeaconsTable: FunctionComponent<IBeaconsTableProps> = React.memo(
+  function ({ beaconsGateway }): JSX.Element {
+    const columns: Column<BeaconTableListRow>[] = React.useMemo(
+      () => [
         {
           title: "Last modified date",
           field: "lastModifiedDate",
@@ -135,73 +133,100 @@ export const BeaconsTable: FunctionComponent<IBeaconsTableProps> = ({
               : "";
           },
         },
-      ]}
-      data={(query) =>
-        new Promise(async (resolve, reject) => {
-          setIsLoading(true);
-          const term = query.search;
-          let statusFilterValue = "";
-          let useFilterValue = "";
-          let sortValue = "";
-          query.filters.forEach((filter) => {
-            if (filter.column.field === "beaconStatus")
-              statusFilterValue = filter.value;
-            if (filter.column.field === "useActivities")
-              useFilterValue = filter.value;
-          });
-          if (query.orderBy) {
-            const sortField = query.orderBy.field;
-            const sortDirection = query.orderDirection;
-            if (sortField && sortDirection) {
-              sortValue = `${sortField},${sortDirection}`;
-            }
-          }
-          try {
-            const response = await beaconsGateway.getAllBeacons(
-              term,
-              statusFilterValue,
-              useFilterValue,
-              query.page,
-              query.pageSize,
-              sortValue
-            );
-            const beacons = response._embedded.beaconSearch.map(
-              (item: IBeaconSearchResultData): BeaconTableListRow => ({
-                lastModifiedDate: item.lastModifiedDate,
-                beaconStatus: item.beaconStatus,
-                hexId: item.hexId,
-                ownerName: item.ownerName ?? "N/A",
-                useActivities: item.useActivities ?? "N/A",
-                id: item.id,
-                beaconType: item.beaconType,
-              })
-            );
-            setIsLoading(false);
-            resolve({
-              data: beacons,
-              page: response.page.number,
-              totalCount: response.page.totalElements,
+        {
+          title: "Manufacturer Serial Number",
+          field: "manufacturerSerialNumber",
+          render: (rowData: BeaconTableListRow) =>
+            replaceNone(rowData.manufacturerSerialNumber),
+        },
+        {
+          title: "Cospas Sarsat Number",
+          field: "cospasSarsatNumber",
+          render: (rowData: BeaconTableListRow) =>
+            replaceNone(rowData.cospasSarsatNumber),
+        },
+        {
+          title: "Serial Number",
+          field: "serialNumber",
+          render: (rowData: BeaconTableListRow) =>
+            replaceNone(rowData.serialNumber),
+        },
+      ],
+      []
+    );
+
+    return (
+      <MaterialTable
+        icons={tableIcons}
+        columns={columns}
+        data={(query) =>
+          new Promise(async (resolve, reject) => {
+            const term = query.search;
+            let statusFilterValue = "";
+            let useFilterValue = "";
+            let sortValue = "";
+            query.filters.forEach((filter) => {
+              if (filter.column.field === "beaconStatus")
+                statusFilterValue = filter.value;
+              if (filter.column.field === "useActivities")
+                useFilterValue = filter.value;
             });
-          } catch (error) {
-            console.error("Could not fetch beacons", error);
-            setIsLoading(false);
-          }
-        })
-      }
-      title=""
-      options={{
-        filtering: true,
-        search: true,
-        searchFieldVariant: "outlined",
-        debounceInterval: 2000,
-        pageSize: 20,
-      }}
-      components={{
-        Container: (props) => <Paper {...props} elevation={0} />,
-        Row: (props) => (
-          <MTableBodyRow {...props} data-testid="beacons-table-row" />
-        ),
-      }}
-    />
-  );
-};
+            if (query.orderBy) {
+              const sortField = query.orderBy.field;
+              const sortDirection = query.orderDirection;
+              if (sortField && sortDirection) {
+                sortValue = `${sortField},${sortDirection}`;
+              }
+            }
+            try {
+              const response = await beaconsGateway.getAllBeacons(
+                term,
+                statusFilterValue,
+                useFilterValue,
+                query.page,
+                query.pageSize,
+                sortValue
+              );
+              const beacons = response._embedded.beaconSearch.map(
+                (item: IBeaconSearchResultData): BeaconTableListRow => ({
+                  lastModifiedDate: item.lastModifiedDate,
+                  beaconStatus: item.beaconStatus,
+                  hexId: item.hexId,
+                  ownerName: item.ownerName ?? "N/A",
+                  useActivities: item.useActivities ?? "N/A",
+                  id: item.id,
+                  beaconType: item.beaconType,
+                  cospasSarsatNumber: item.cospasSarsatNumber ?? "N/A",
+                  manufacturerSerialNumber:
+                    item.manufacturerSerialNumber ?? "N/A",
+                  serialNumber: item.serialNumber ?? "N/A",
+                })
+              );
+              resolve({
+                data: beacons,
+                page: response.page.number,
+                totalCount: response.page.totalElements,
+              });
+            } catch (error) {
+              console.error("Could not fetch beacons", error);
+            }
+          })
+        }
+        title=""
+        options={{
+          filtering: true,
+          search: true,
+          searchFieldVariant: "outlined",
+          debounceInterval: 1000,
+          pageSize: 20,
+        }}
+        components={{
+          Container: (props) => <Paper {...props} elevation={0} />,
+          Row: (props) => (
+            <MTableBodyRow {...props} data-testid="beacons-table-row" />
+          ),
+        }}
+      />
+    );
+  }
+);
