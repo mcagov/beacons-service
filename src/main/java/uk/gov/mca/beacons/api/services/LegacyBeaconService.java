4a50fbb425a04b2c081f1cc30a1f54e55b44a57e
Olly Swanson <olly.swanson95@gmail.com>
2021-11-12 11:26:35 +0000

    LegacyBeaconService::claim does not attempt to save the beacon if the legacyBeacon has been claimed previously

--- src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
+++ src/main/java/uk/gov/mca/beacons/api/services/LegacyBeaconService.java
@@ -78,8 +78,8 @@ public class LegacyBeaconService {
       legacyBeacon.getAssociatedEmailAddress()
     );
 
-    legacyBeacon.claimFor(accountHolder);
-
-    legacyBeaconGateway.save(legacyBeacon);
+    if (legacyBeacon.claimFor(accountHolder)) {
+      legacyBeaconGateway.save(legacyBeacon);
+    }
   }
 }
