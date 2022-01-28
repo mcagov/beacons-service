23399144d70b6e7a17168e23797a7c2fed2144c9
Olly Swanson <olly.swanson95@gmail.com>
2022-01-28 11:53:00 +0000

    Created BeaconBatchJob for sending BeaconSearchDocuments to Opensearch, reading domain objects from database.

---
+++ src/main/java/uk/gov/mca/beacons/api/search/jobs/BeaconBatchJobConfiguration.java
@@ -0,0 +1,73 @@
+package uk.gov.mca.beacons.api.search.jobs;
+
+import javax.persistence.EntityManagerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.mca.beacons.api.beacon.domain.Beacon;
+import uk.gov.mca.beacons.api.search.documents.BeaconSearchDocument;
+
+@Configuration
+@EnableBatchProcessing
+public class BeaconBatchJobConfiguration {
+
+  private final JobBuilderFactory jobBuilderFactory;
+  private final StepBuilderFactory stepBuilderFactory;
+  private final EntityManagerFactory entityManagerFactory;
+  private static final int chunkSize = 256;
+
+  @Autowired
+  public BeaconBatchJobConfiguration(
+    JobBuilderFactory jobBuilderFactory,
+    StepBuilderFactory stepBuilderFactory,
+    EntityManagerFactory entityManagerFactory
+  ) {
+    this.jobBuilderFactory = jobBuilderFactory;
+    this.stepBuilderFactory = stepBuilderFactory;
+    this.entityManagerFactory = entityManagerFactory;
+  }
+
+  @Bean
+  public JpaPagingItemReader<Beacon> itemReader() {
+    return new JpaPagingItemReaderBuilder<Beacon>()
+      .name("beaconReader")
+      .entityManagerFactory(entityManagerFactory)
+      .queryString("select b from beacon b order by lastModifiedDate")
+      .pageSize(chunkSize)
+      .build();
+  }
+
+  @Bean
+  public Step sendBeaconSearchDocumentStep(
+    ItemWriter<BeaconSearchDocument> beaconSearchDocumentWriter,
+    ItemProcessor<Beacon, BeaconSearchDocument> beaconBatchJobProcessor
+  ) {
+    return stepBuilderFactory
+      .get("sendBeaconSearchDocumentStep")
+      .<Beacon, BeaconSearchDocument>chunk(chunkSize)
+      .reader(itemReader())
+      .processor(beaconBatchJobProcessor)
+      .writer(beaconSearchDocumentWriter)
+      .build();
+  }
+
+  @Bean
+  public Job sendBeaconSearchDocumentJob(Step sendBeaconSearchDocumentStep) {
+    return jobBuilderFactory
+      .get("sendBeaconSearchDocumentStep")
+      .incrementer(new RunIdIncrementer())
+      .flow(sendBeaconSearchDocumentStep)
+      .end()
+      .build();
+  }
+}
---
+++ src/main/java/uk/gov/mca/beacons/api/search/jobs/BeaconSearchDocumentWriter.java
@@ -0,0 +1,29 @@
+package uk.gov.mca.beacons.api.search.jobs;
+
+import java.util.List;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import uk.gov.mca.beacons.api.search.documents.BeaconSearchDocument;
+import uk.gov.mca.beacons.api.search.repositories.BeaconSearchRepository;
+
+@Component
+public class BeaconSearchDocumentWriter
+  implements ItemWriter<BeaconSearchDocument> {
+
+  private final BeaconSearchRepository beaconSearchRepository;
+
+  @Autowired
+  public BeaconSearchDocumentWriter(
+    BeaconSearchRepository beaconSearchRepository
+  ) {
+    this.beaconSearchRepository = beaconSearchRepository;
+  }
+
+  @Override
+  public void write(@NonNull List<? extends BeaconSearchDocument> documents)
+    throws Exception {
+    beaconSearchRepository.saveAll(documents);
+  }
+}
