94c7c47a6890a2e9b7745966e70e01ab1ee49d27
Olly Swanson <olly.swanson95@gmail.com>
2022-02-01 13:22:04 +0000

    When a job is started by posting to the job endpoint a JobAcceptanceDTO is returned to the caller detailing the location of the endpoint to retrieve a JobOutputDTO with meta information about the long running task.

---
+++ src/main/java/uk/gov/mca/beacons/api/jobs/rest/JobAcceptanceDTO.java
@@ -0,0 +1,13 @@
+package uk.gov.mca.beacons.api.jobs.rest;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobAcceptanceDTO {
+
+  private String location;
+}
