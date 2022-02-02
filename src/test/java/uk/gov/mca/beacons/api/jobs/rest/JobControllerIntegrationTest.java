f78c149e00b5d45a0167c76953491745d8527d61
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-02 16:59:54 +0000

    Test that LegacyBeacons are also reindexed in batch job

--- src/test/java/uk/gov/mca/beacons/api/jobs/rest/JobControllerIntegrationTest.java
+++ src/test/java/uk/gov/mca/beacons/api/jobs/rest/JobControllerIntegrationTest.java
@@ -4,35 +4,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import com.jayway.jsonpath.JsonPath;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.*;
-import org.elasticsearch.action.get.GetRequest;
+import java.util.concurrent.TimeUnit;
+import javax.batch.runtime.BatchStatus;
 import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.client.Request;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.FieldSortBuilder;
-import org.elasticsearch.search.sort.SortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.query.Query;
-import org.springframework.data.elasticsearch.core.query.StringQuery;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
 import uk.gov.mca.beacons.api.WebIntegrationTest;
-import uk.gov.mca.beacons.api.search.documents.BeaconSearchDocument;
-import uk.gov.mca.beacons.api.search.repositories.BeaconSearchRepository;
 
 @SpringBatchTest
 public class JobControllerIntegrationTest extends WebIntegrationTest {
@@ -50,13 +36,23 @@ public class JobControllerIntegrationTest extends WebIntegrationTest {
   }
 
   @Test
-  void givenBeacons_whenJobIsTriggeredViaEndpoint_ShouldWriteBeaconSearchDocumentsToOpensearch()
+  void whenJobIsTriggeredViaEndpoint_ShouldWriteBeaconSearchDocumentsToOpensearch()
     throws Exception {
     // given
     String accountHolderId_1 = seedAccountHolder();
     String accountHolderId_2 = seedAccountHolder();
-    String seededRegistrationId_1 = seedRegistration(RegistrationUseCase.SINGLE_BEACON, accountHolderId_1);
-    String seededRegistrationId_2 = seedRegistration(RegistrationUseCase.SINGLE_BEACON, accountHolderId_2);
+    String seededRegistrationId_1 = seedLegacyBeacon();
+    String seededRegistrationId_2 = seedLegacyBeacon(
+      fixture -> fixture.replace("2004-10-13T00:00:00", "2014-10-13T00:00:00")
+    );
+    String seededRegistrationId_3 = seedRegistration(
+      RegistrationUseCase.SINGLE_BEACON,
+      accountHolderId_1
+    );
+    String seededRegistrationId_4 = seedRegistration(
+      RegistrationUseCase.SINGLE_BEACON,
+      accountHolderId_2
+    );
 
     // when
     webTestClient
@@ -69,44 +65,49 @@ public class JobControllerIntegrationTest extends WebIntegrationTest {
       .jsonPath("$.location")
       .isEqualTo("/spring-api/job/reindexSearch/1");
 
-    pollJobStatusUntilCompletedOrFailed(
-      Endpoints.Job.value + "/reindexSearch/1"
-    );
+    pollJobStatusUntilFinished(Endpoints.Job.value + "/reindexSearch/1");
 
     // then
     SearchRequest searchRequest = new SearchRequest("beacon_search");
     SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-    searchSourceBuilder.query(QueryBuilders.matchAllQuery()).sort(new FieldSortBuilder("lastModifiedDate").order(SortOrder.ASC));
+    searchSourceBuilder
+      .query(QueryBuilders.matchAllQuery())
+      .sort(new FieldSortBuilder("lastModifiedDate").order(SortOrder.ASC));
     searchRequest.source(searchSourceBuilder);
 
-    var response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+    var response = restHighLevelClient.search(
+      searchRequest,
+      RequestOptions.DEFAULT
+    );
 
     assertThat(response.getHits().getAt(0).getId(), is(seededRegistrationId_1));
     assertThat(response.getHits().getAt(1).getId(), is(seededRegistrationId_2));
+    assertThat(response.getHits().getAt(2).getId(), is(seededRegistrationId_3));
+    assertThat(response.getHits().getAt(3).getId(), is(seededRegistrationId_4));
   }
 
-  private void pollJobStatusUntilCompletedOrFailed(String endpoint) {
-    Flux
-      .just(webTestClient.get().uri(endpoint).exchange())
-      .flatMap(
-        clientResponse -> {
-          String status = JsonPath.read(
-            clientResponse
-              .returnResult(String.class)
-              .getResponseBody()
-              .blockFirst(),
-            "$.status"
-          );
+  private void pollJobStatusUntilFinished(String endpoint)
+    throws InterruptedException {
+    int maxRetries = 10;
 
-          assertThat(status, not("FAILED"));
+    for (int i = 0; i < maxRetries; i++) {
+      String status = JsonPath.read(
+        webTestClient
+          .get()
+          .uri(endpoint)
+          .exchange()
+          .returnResult(String.class)
+          .getResponseBody()
+          .blockFirst(),
+        "$.status"
+      );
 
-          if (!status.equals("COMPLETED")) {
-            return Mono.error(new RuntimeException());
-          }
-          return Mono.just(status);
-        }
-      )
-      .retryWhen(Retry.fixedDelay(10, Duration.ofSeconds(1)))
-      .blockFirst();
+      assert !status.equals(BatchStatus.FAILED.toString());
+      assert !status.equals(BatchStatus.ABANDONED.toString());
+
+      if (status.equals("COMPLETED")) break;
+
+      TimeUnit.SECONDS.sleep(1);
+    }
   }
 }
