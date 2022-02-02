c33740bb3da132c1b82e25aaf47d5fb6aaae3c77
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-02 13:09:55 +0000

    Add constructor that handles LegacyUses
    For use in constructing BeaconSearch documents from LegacyBeacons.

--- src/main/java/uk/gov/mca/beacons/api/search/documents/nested/NestedBeaconUse.java
+++ src/main/java/uk/gov/mca/beacons/api/search/documents/nested/NestedBeaconUse.java
@@ -3,6 +3,7 @@ package uk.gov.mca.beacons.api.search.documents.nested;
 import lombok.Getter;
 import lombok.Setter;
 import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUse;
+import uk.gov.mca.beacons.api.legacybeacon.domain.LegacyUse;
 
 @Getter
 @Setter
@@ -12,6 +13,10 @@ public class NestedBeaconUse {
 
   public NestedBeaconUse(BeaconUse beaconUse) {
     this.environment = beaconUse.getEnvironment().toString();
+  }
+
+  public NestedBeaconUse(LegacyUse legacyUse) {
+    this.environment = legacyUse.getEnvironment();
   }
 
   private String environment;
