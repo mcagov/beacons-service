40b0d79acefffbc287bd215c8935662a67d3b5db
Olly Swanson <olly.swanson95@gmail.com>
2022-02-14 10:09:18 +0000

    Changed ownerName from SearchAsYouType to Keyword.
    SearchAsYouType did not have the desired effect when used in confunction
    with Opensearch Dashboard

--- src/main/java/uk/gov/mca/beacons/api/search/documents/nested/NestedBeaconOwner.java
+++ src/main/java/uk/gov/mca/beacons/api/search/documents/nested/NestedBeaconOwner.java
@@ -21,6 +21,6 @@ public class NestedBeaconOwner {
     this.ownerName = legacyOwner.getOwnerName();
   }
 
-  @Field(type = FieldType.Search_As_You_Type)
+  @Field(type = FieldType.Keyword)
   private String ownerName;
 }
