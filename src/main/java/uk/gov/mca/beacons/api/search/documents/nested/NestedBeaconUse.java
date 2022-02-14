c42f98c59e3ac3a09da31bdb6004a37b34936db2
Olly Swanson <olly.swanson95@gmail.com>
2022-02-14 13:00:10 +0000

    Add null checks to setters.

--- src/main/java/uk/gov/mca/beacons/api/search/documents/nested/NestedBeaconUse.java
+++ src/main/java/uk/gov/mca/beacons/api/search/documents/nested/NestedBeaconUse.java
@@ -4,7 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import uk.gov.mca.beacons.api.beaconuse.domain.Activity;
 import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUse;
+import uk.gov.mca.beacons.api.beaconuse.domain.Environment;
+import uk.gov.mca.beacons.api.beaconuse.domain.Purpose;
 import uk.gov.mca.beacons.api.legacybeacon.domain.LegacyUse;
 
 @Getter
@@ -14,18 +17,18 @@ public class NestedBeaconUse {
   public NestedBeaconUse() {}
 
   public NestedBeaconUse(BeaconUse beaconUse) {
-    this.environment = beaconUse.getEnvironment().toString().toUpperCase();
-    this.activity = beaconUse.getActivity().toString().toUpperCase();
-    this.purpose = beaconUse.getPurpose().toString().toUpperCase();
+    setEnvironment(beaconUse.getEnvironment());
+    setActivity(beaconUse.getActivity());
+    setPurpose(beaconUse.getPurpose());
     this.vesselName = beaconUse.getVesselName();
     this.callSign = beaconUse.getCallSign();
     this.aircraftRegistrationMark = beaconUse.getRegistrationMark();
   }
 
   public NestedBeaconUse(LegacyUse legacyUse) {
-    this.environment = legacyUse.getEnvironment();
-    this.activity = legacyUse.getActivity();
-    this.purpose = legacyUse.getPurpose();
+    setEnvironment(legacyUse.getEnvironment());
+    setActivity(legacyUse.getActivity());
+    setPurpose(legacyUse.getPurpose());
     this.vesselName = legacyUse.getVesselName();
     this.callSign = legacyUse.getCallSign();
     this.aircraftRegistrationMark = legacyUse.getAircraftRegistrationMark();
@@ -48,4 +51,52 @@ public class NestedBeaconUse {
 
   @Field(type = FieldType.Keyword)
   private String aircraftRegistrationMark;
+
+  public void setEnvironment(Environment environment) {
+    if (environment != null) {
+      this.environment = environment.toString().toUpperCase();
+    } else {
+      this.environment = null;
+    }
+  }
+
+  public void setEnvironment(String environment) {
+    if (environment != null) {
+      this.environment = environment.toUpperCase();
+    } else {
+      this.environment = null;
+    }
+  }
+
+  public void setPurpose(Purpose purpose) {
+    if (purpose != null) {
+      this.purpose = purpose.toString().toUpperCase();
+    } else {
+      this.purpose = null;
+    }
+  }
+
+  public void setPurpose(String purpose) {
+    if (purpose != null) {
+      this.purpose = purpose.toUpperCase();
+    } else {
+      this.purpose = null;
+    }
+  }
+
+  public void setActivity(Activity activity) {
+    if (activity != null) {
+      this.activity = activity.toString().toUpperCase();
+    } else {
+      this.activity = null;
+    }
+  }
+
+  public void setActivity(String activity) {
+    if (activity != null) {
+      this.activity = activity.toUpperCase();
+    } else {
+      this.activity = null;
+    }
+  }
 }
